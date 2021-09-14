d98c5827b162d994aafb44075e4ec90c64882b3a
Shibo Tong <shibo0617@gmail.com>
2021-09-15 00:06:32 +1000

    update privacy policy

--- App/Views/SideBarView/AboutUsView.swift
+++ App/Views/SideBarView/AboutUsView.swift
@@ -18,7 +18,7 @@ struct AboutUsView: View {
                 Section(header: Text("Our App").font(.custom(fontString, size: 15))) {
                 makeRow(image: "chevron.left.slash.chevron.right", text: "Source Code / Report an Issue", link: URL(string: "https://github.com/shibotong/Dota2Armory"))
                 makeRow(image: "star", text: "Rate the app on App Store", link: URL(string: ""))
-                makeRow(image: "lock", text: "Privacy Policy", link: URL(string: ""))
+                makeRow(image: "lock", text: "Privacy Policy", link: URL(string: "https://github.com/shibotong/Dota2Armory/blob/main/documents/privacy-policy.md"))
                 makeRow(image: "person", text: "Term of Use", link: URL(string: ""))
                 makeDetailRow(image: "app.badge", text: "App Version", detail: versionNumber)
                 makeDetailRow(image: "gamecontroller", text: "Game Patch", detail: gameVersion)
