bdc20d3ea884ee6fb4d0f4747d7e583dd47e2917
Shibo Tong <shibo0617@gmail.com>
2022-06-15 17:04:31 +1000

    update image

--- App/Views/HeroImageView.swift
+++ App/Views/HeroImageView.swift
@@ -38,21 +38,18 @@ struct HeroImageView: View {
     }
     
     private func searchHeroImage() -> String {
-        guard let hero = HeroDatabase.shared.fetchHeroWithID(id: heroID) else {
-            return ""
-        }
         switch self.type {
         case .icon:
-            let filename = "\(hero.localizedName)_icon"
+            let filename = "\(heroID.description)_icon"
             return filename
         case .portrait:
-            let filename = "\(hero.localizedName)_portrait"
+            let filename = "\(heroID.description)_portrait"
             return filename
         case .full:
-            let filename = "\(hero.localizedName)_full"
+            let filename = "\(heroID.description)_full"
             return filename
         case .vert:
-            let filename = "\(hero.localizedName)_vert"
+            let filename = "\(heroID.description)_vert"
             return filename
         }
     }
