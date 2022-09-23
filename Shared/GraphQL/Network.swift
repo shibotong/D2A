689ce8610039dc913120da54c7461c8b8bfece29
Shibo Tong <shibo0617@gmail.com>
2022-09-23 12:08:33 +0800

    GraphQL with Hero Talents

--- Shared/GraphQL/Network.swift
+++ Shared/GraphQL/Network.swift
@@ -11,7 +11,7 @@ import Apollo
 class Network {
   static let shared = Network()
     private(set) lazy var apollo = {
-        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJodHRwczovL3N0ZWFtY29tbXVuaXR5LmNvbS9vcGVuaWQvaWQvNzY1NjExOTgxMTMzMDc2ODUiLCJ1bmlxdWVfbmFtZSI6Ik1yLkJPQk9CTyIsIlN1YmplY3QiOiIwMTg0NGZiOS01ZDZjLTQ2MGEtYjRmZS04OTJjNzgwZjVjMTciLCJTdGVhbUlkIjoiMTUzMDQxOTU3IiwibmJmIjoxNjYzODE4NTU4LCJleHAiOjE2OTUzNTQ1NTgsImlhdCI6MTY2MzgxODU1OCwiaXNzIjoiaHR0cHM6Ly9hcGkuc3RyYXR6LmNvbSJ9.9yUJodq--fWTzs2lWdTULjUayX7djl6hhIqsu0TNvcw"
+        let token = Token.stratzToken
         let url = URL(string: "https://api.stratz.com/graphql")!
         
         let additionalHeaders = ["Authorization": "Bearer \(token)"]
