e63b35fdef5137fbf56a592d3888c58771cc6cad
Shibo Tong <shibo0617@gmail.com>
2022-09-26 10:03:33 +1000

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,12 +6,9 @@ brew install cocoapods
 # Install dependencies you manage with CocoaPods.
 pod install
 
-echo 'This is token'
-echo $STRATZTOKEN
-echo $PWD
 # Add Secret
 echo '
 {
     "stratzToken": "${STRATZTOKEN}"
 }
-' >> Shared/GraphQL/secrets.json
+' >> ../Shared/GraphQL/secrets.json
