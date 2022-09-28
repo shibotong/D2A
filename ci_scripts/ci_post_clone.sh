3b6f37f7d05ce9a735645a393cc127fc642624ad
Shibo Tong <shibo0617@gmail.com>
2022-09-28 12:07:27 +1000

    make secrets in one line

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,12 +7,4 @@ brew install cocoapods
 pod install
 
 # Add Secret
-echo '
-{
-    "stratzToken": "'"$STRATZTOKEN"'"
-}
-' >> ../Shared/GraphQL/secrets.json
-
-
-echo $PWD
-cat ../Shared/GraphQL/secrets.json
+echo '{ "stratzToken": "'"$STRATZTOKEN"'" }' >> ../Shared/GraphQL/secrets.json
