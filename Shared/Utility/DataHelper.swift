f1abc0e92bd42435ae46eb88e0195079c44894ec
Shibo Tong <shibo0617@gmail.com>
2022-11-14 09:16:58 +1100

    add localization for rank

--- Shared/Utility/DataHelper.swift
+++ Shared/Utility/DataHelper.swift
@@ -6,28 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct DataHelper {
-    static func transferRank(rank: Int?) -> String {
+    static func transferRank(rank: Int?) -> LocalizedStringKey {
         if let rank = rank {
-            var level = ""
-            if rank >= 80 {
-                return "Immortal"
-            } else if rank > 70 {
-                level = "Divine"
-            } else if rank > 60 {
-                level = "Ancient"
-            } else if rank > 50 {
-                level = "Legend"
-            } else if rank > 40 {
-                level = "Archon"
-            } else if rank > 30 {
-                level = "Crusader"
-            } else if rank > 20 {
-                level = "Guardian"
-            } else if rank > 10 {
-                level = "Herald"
-            }
             let number = rank % 10
             var numberRoman = ""
             switch number {
@@ -38,10 +21,28 @@ struct DataHelper {
             case 5: numberRoman = "V"
             default: numberRoman = ""
             }
-            return "\(level) \(numberRoman)"
             
+            if rank >= 80 {
+                return LocalizedStringKey("Immortal")
+            } else if rank > 70 {
+                return LocalizedStringKey("Divine \(numberRoman)")
+            } else if rank > 60 {
+                return LocalizedStringKey("Ancient \(numberRoman)")
+            } else if rank > 50 {
+                return LocalizedStringKey("Legend \(numberRoman)")
+            } else if rank > 40 {
+                return LocalizedStringKey("Archon \(numberRoman)")
+            } else if rank > 30 {
+                return LocalizedStringKey("Crusader \(numberRoman)")
+            } else if rank > 20 {
+                return LocalizedStringKey("Guardian \(numberRoman)")
+            } else if rank > 10 {
+                return LocalizedStringKey("Herald \(numberRoman)")
+            } else {
+                return LocalizedStringKey("Unranked")
+            }
         } else {
-            return "Unranked"
+            return LocalizedStringKey("Unranked")
         }
     }
 }
