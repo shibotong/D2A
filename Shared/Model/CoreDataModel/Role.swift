c9ba6f6e15634c1026ca7d65c8f59ad11b02d23e
Shibo Tong <shibo0617@gmail.com>
2022-11-28 11:38:07 +1100

    Update CoreData

--- Shared/Model/CoreDataModel/Role.swift
+++ Shared/Model/CoreDataModel/Role.swift
@@ -25,14 +25,7 @@ extension Role {
         }
         newRole.roleId = roleID.rawValue
         newRole.level = level
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
+        try viewContext.save()
         return newRole
     }
 }
