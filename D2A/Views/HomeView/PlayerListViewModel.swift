88dae16ce9bd6062176e344a5883bcad6139d46c
Shibo Tong <shibo0617@gmail.com>
2022-12-03 11:56:59 +1100

    Updater UI

--- D2A/Views/HomeView/PlayerListViewModel.swift
+++ D2A/Views/HomeView/PlayerListViewModel.swift
@@ -8,8 +8,9 @@
 import Foundation
 
 class PlayerListViewModel: ObservableObject {
-    @Published var registerdID: String
     @Published var userIDs: [String]
+    
+    @Published var registerdID: String
     
     init(registeredID: String, followedID: [String]) {
         self.registerdID = registeredID
