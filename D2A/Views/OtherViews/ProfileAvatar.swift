486a6709820a2b49581903a94b697ab3de04d9d8
Shibo Tong <shibo0617@gmail.com>
2022-12-11 17:05:56 +1100

    update avatar

--- D2A/Views/OtherViews/ProfileAvatar.swift
+++ D2A/Views/OtherViews/ProfileAvatar.swift
@@ -9,24 +9,16 @@ import SwiftUI
 
 struct ProfileAvartar: View {
     
+    @Environment(\.managedObjectContext) private var viewContext
     @State var image: UIImage?
     
-    let urlString: String
-    let userid: Int
+    let profile: UserProfile
     
     let sideLength: CGFloat
     let cornerRadius: CGFloat
     
     init(profile: UserProfile, sideLength: CGFloat, cornerRadius: CGFloat) {
-        self.urlString = profile.avatarfull ?? ""
-        self.userid = Int(profile.id ?? "0") ?? 0
-        self.sideLength = sideLength
-        self.cornerRadius = cornerRadius
-    }
-    
-    init(userid: Int, urlString: String, sideLength: CGFloat, cornerRadius: CGFloat) {
-        self.urlString = urlString
-        self.userid = userid
+        self.profile = profile
         self.sideLength = sideLength
         self.cornerRadius = cornerRadius
     }
@@ -50,13 +42,33 @@ struct ProfileAvartar: View {
     }
     
     private func fetchImage() async {
-        let cache = ImageCache.shared
-        guard let url = URL(string: urlString) else {
+        guard let userid = profile.id else {
             return
         }
-        let image = try? await cache.fetchImage(type: .avatar, id: userid, url: url)
-        self.image = image
+        let cacheImage = ImageCache.readImage(type: .avatar, id: userid)
+        Dispatch.DispatchQueue.main.async {
+            image = cacheImage
+        }
+        if cacheImage == nil || profile.lastUpdate?.startOfDay != Date().startOfDay {
+            guard let newImage = await loadImage() else {
+                return
+            }
+            ImageCache.saveImage(newImage, type: .avatar, id: userid)
+            profile.lastUpdate = Date()
+            try? viewContext.save()
+            DispatchQueue.main.async {
+                image = newImage
+            }
+        }
     }
     
-    
+    private func loadImage() async -> UIImage? {
+        guard let urlString = profile.avatarfull,
+              let url = URL(string: urlString),
+              let (newImageData, _) = try? await URLSession.shared.data(from: url),
+              let newImage = UIImage(data: newImageData) else {
+            return nil
+        }
+        return newImage
+    }
 }
