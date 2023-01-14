d0f26f90f5892b6ce5e34e131f2d0b6a973cbc1f
Shibo Tong <shibo0617@gmail.com>
2023-01-15 10:56:04 +1100

    Update D2AWidget.swift

--- D2AWidget/D2AWidget.swift
+++ D2AWidget/D2AWidget.swift
@@ -1,58 +1,58 @@
-////
-////  D2AWidget.swift
-////  D2AWidget
-////
-////  Created by Shibo Tong on 18/9/21.
-////
 //
-//import WidgetKit
-//import SwiftUI
-//import Intents
+//  D2AWidget.swift
+//  D2AWidget
 //
-//@main
-//struct D2AWidget: WidgetBundle {
-//    @WidgetBundleBuilder
-//    var body: some Widget {
-//        RecentMatchesWidget()
-//        LatestMatchWidget()
-//    }
-//}
+//  Created by Shibo Tong on 18/9/21.
 //
-//
-//struct RecentMatchesWidget: Widget {
-//    let kind: String = "D2AWidget"
-//    var body: some WidgetConfiguration {
-//        IntentConfiguration(kind: kind, intent: DynamicUserSelectionIntent.self, provider: Provider()) { entry in
-////            RecentMatchesEntryView(entry: entry)
-//            RecentMatchesWidgetEntryView(entry: entry)
-//        }
-//        .configurationDisplayName("Recent Matches")
-//        .description("Your recent matches.")
-//        .supportedFamilies([.systemSmall, .systemMedium])
-//    }
-//}
-//
-//struct LatestMatchWidget: Widget {
-//    let kind: String = "LatestWidget"
-//    var body: some WidgetConfiguration {
-//        IntentConfiguration(kind: kind, intent: DynamicUserSelectionIntent.self, provider: Provider()) { entry in
-////            RecentMatchesEntryView(entry: entry)
-//            LatestMatchWidgetEntryView(entry: entry)
-//        }
-//        .configurationDisplayName("Latest Match")
-//        .description("Your latest match.")
-//        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
-//        
-//    }
-//}
-//
-//
-//
-//struct SimpleEntry: TimelineEntry {
-//    let date: Date
-//    let matches: [RecentMatch]
-//    let user: UserProfile?
-//    let subscription: Bool
-//}
-//
-//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+@main
+struct D2AWidget: WidgetBundle {
+    @WidgetBundleBuilder
+    var body: some Widget {
+        RecentMatchesWidget()
+        LatestMatchWidget()
+    }
+}
+
+
+struct RecentMatchesWidget: Widget {
+    let kind: String = "D2AWidget"
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: DynamicUserSelectionIntent.self, provider: Provider()) { entry in
+//            RecentMatchesEntryView(entry: entry)
+            RecentMatchesWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Recent Matches")
+        .description("Your recent matches.")
+        .supportedFamilies([.systemSmall, .systemMedium])
+    }
+}
+
+struct LatestMatchWidget: Widget {
+    let kind: String = "LatestWidget"
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: DynamicUserSelectionIntent.self, provider: Provider()) { entry in
+//            RecentMatchesEntryView(entry: entry)
+            LatestMatchWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Latest Match")
+        .description("Your latest match.")
+        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
+        
+    }
+}
+
+
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let matches: [RecentMatch]
+    let user: UserProfile?
+    let subscription: Bool
+}
+
+
