eba81c3f3269ec7007ef0e537272689521a04318
shibotong <shibo0617@gmail.com>
2023-02-26 14:05:22 +1100

    Update Ability image

--- D2A/Views/OtherViews/AbilityImage.swift
+++ D2A/Views/OtherViews/AbilityImage.swift
@@ -9,52 +9,23 @@ import SwiftUI
 import UIKit
 
 struct AbilityImage: View {
-    var name: String
-    var urlString: String
-    let sideLength: CGFloat
-    let cornerRadius: CGFloat
     
-    @State var image: UIImage?
+    @ObservedObject var viewModel: AbilityImageViewModel
     
     var body: some View {
         ZStack {
-            if let image = image {
+            EmptyView()
+            if let image = viewModel.image {
                 Image(uiImage: image)
                     .resizable()
             } else {
                 Image("ability_slot")
                     .renderingMode(.template)
                     .resizable()
+                    .foregroundColor(.label)
             }
         }
-        .frame(width: sideLength, height: sideLength)
-        .task {
-            await fetchImage()
-        }
-    }
-    
-    private func fetchImage() async {
-        if let cacheImage = ImageCache.readImage(type: .ability, id: name) {
-            Dispatch.DispatchQueue.main.async {
-                image = cacheImage
-            }
-            return
-        }
-        guard let newImage = await loadImage() else {
-            return
-        }
-        ImageCache.saveImage(newImage, type: .ability, id: name)
-        DispatchQueue.main.async {
-            image = newImage
-        }
-    }
-    
-    private func loadImage() async -> UIImage? {
-        guard let url = URL(string: urlString),
-              let (newImageData, _) = try? await URLSession.shared.data(from: url),
-              let newImage = UIImage(data: newImageData) else {
-            return nil
-        }
-        return newImage
+        .frame(width: viewModel.sideLength, height: viewModel.sideLength)
+        .cornerRadius(viewModel.cornerRadius)
     }
 }
