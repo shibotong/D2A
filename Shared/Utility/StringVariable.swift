f928fcca60c287b7aed596dfbcaf17aab1693c53
shibotong <shibo0617@gmail.com>
2023-03-13 13:39:28 +1100

    Update dev team

--- Shared/Utility/StringVariable.swift
+++ Shared/Utility/StringVariable.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 let productIDs = ["D2APRO"]// ["D2APlusMonthly", "D2APlusQuarterly", "D2APlusAnnually"]
-let GROUP_NAME = "group.D2A"
+let GROUP_NAME = "group.com.shibotong.D2A"
 let TERMS_OF_USE = "https://github.com/shibotong/Dota2Armory/blob/main/Shared/documents/terms-of-use.md"
 let PRIVACY_POLICY = "https://github.com/shibotong/Dota2Armory/blob/main/Shared/documents/privacy-policy.md"
 let currentLanguage: String = Locale.current.languageCode ?? "en"
