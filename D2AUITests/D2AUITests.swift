721f1299388b53a21dc02bdc32420064611c26a0
Shibo Tong <shibo0617@gmail.com>
2023-04-16 10:19:51 +1000

    Add UI Screenshot pipeline (#31)
    * add uitest target
    
    * add uitest
    
    * add uitests for screenshot
    
    * Update .swiftlint.yml
    
    * update system setting
    
    * added test rank view

--- D2AUITests/D2AUITests.swift
+++ D2AUITests/D2AUITests.swift
@@ -35,7 +35,6 @@ final class D2AUITests: XCTestCase {
         app.buttons["Register Player"].tap()
         sleep(networkWaiting)
         XCTAssert(app.staticTexts["Mr.BOBOBO"].exists)
-        takeScreenshot("Home")
     }
     
     /// Test all hero page
@@ -54,38 +53,6 @@ final class D2AUITests: XCTestCase {
             sleep(networkWaiting)
             takeScreenshot("Ability Detail")
         }
-    }
-    
-    /// Test searching function of app
-    func testSearch() {
-        let app = startApp()
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            app.buttons["Search"].tap()
-        } else {
-            app.tabBars["Tab Bar"].buttons["Search"].tap()
-        }
-        let textField = app.searchFields["Players, Heroes, Matches"]
-        textField.tap()
-        textField.typeText("Mr.BOBOBO")
-        sleep(networkWaiting)
-        // click enter on software keyboard
-        app.keyboards.buttons["search"].tap()
-        sleep(networkWaiting)
-        XCTAssert(app.staticTexts["Mr.BOBOBO"].exists)
-        takeScreenshot("Search")
-        app.buttons["153041957"].tap()
-        sleep(networkWaiting)
-        takeScreenshot("Profile")
-        app.buttons["All"].tap()
-        app.buttons["Month"].tap()
-        let datePicker = app.datePickers
-        datePicker.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "March")
-        datePicker.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "2023")
-        app.buttons["Month"].tap()
-        app.buttons["Thursday, March 30"].tap()
-        app.buttons["7084211966"].tap()
-        sleep(networkWaiting)
-        takeScreenshot("Match Detail")
     }
     
     /// Test app user to favourite to app
@@ -112,6 +79,59 @@ final class D2AUITests: XCTestCase {
             app.tabBars["Tab Bar"].buttons["Home"].tap()
         }
         XCTAssert(app.staticTexts["Mr.BOBOBO"].exists)
+    }
+    
+    /// Take screenshots for each devices
+    func testCaptureSnapshots() {
+        // Take screenshot on home page
+        let app = startApp()
+        let textField = app.textFields["Search ID"]
+        textField.tap()
+        textField.typeText("153041957")
+        app.buttons["Register Player"].tap()
+        sleep(networkWaiting)
+        takeScreenshot("Home")
+        
+        // Take screenshot for Hero List
+        app.buttons["Heroes"].tap()
+        let abaddonButton = app.buttons["Abaddon"]
+        takeScreenshot("Hero List")
+        abaddonButton.tap()
+        let borrowedTimeButton = app.buttons["abaddon_borrowed_time"]
+        takeScreenshot("Hero Detail")
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            borrowedTimeButton.tap()
+            sleep(networkWaiting)
+            takeScreenshot("Ability Detail")
+        }
+        
+        // Take screenshot for search page
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            app.buttons["Search"].tap()
+        } else {
+            app.tabBars["Tab Bar"].buttons["Search"].tap()
+        }
+        let searchTextField = app.searchFields["Players, Heroes, Matches"]
+        searchTextField.tap()
+        searchTextField.typeText("Mr.BOBOBO")
+        sleep(networkWaiting)
+        // click enter on software keyboard
+        app.keyboards.buttons["search"].tap()
+        sleep(networkWaiting)
+        takeScreenshot("Search")
+        app.buttons["153041957"].tap()
+        sleep(networkWaiting)
+        takeScreenshot("Profile")
+        app.buttons["All"].tap()
+        app.buttons["Month"].tap()
+        let datePicker = app.datePickers
+        datePicker.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "March")
+        datePicker.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "2023")
+        app.buttons["Month"].tap()
+        app.buttons["Thursday, March 30"].tap()
+        app.buttons["7084211966"].tap()
+        sleep(networkWaiting)
+        takeScreenshot("Match Detail")
     }
     
     private func startApp() -> XCUIApplication {
