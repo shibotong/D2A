fc8d048899a7ceeb77733f465e4780d1a28e55f0
shibotong <shibo0617@gmail.com>
2023-06-10 00:41:07 +1000

    add live match history query

---
+++ StratzAPI/Sources/Schema/Objects/LiveQuery.graphql.swift
@@ -0,0 +1,11 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public extension Objects {
+  static let LiveQuery = Object(
+    typename: "LiveQuery",
+    implementedInterfaces: []
+  )
+}
