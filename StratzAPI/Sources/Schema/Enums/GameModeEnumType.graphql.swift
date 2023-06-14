e89ac2529ea4d78c2f6abef7503588611add47ec
shibotong <shibo0617@gmail.com>
2023-06-14 22:10:48 +1000

    update draft UI

---
+++ StratzAPI/Sources/Schema/Enums/GameModeEnumType.graphql.swift
@@ -0,0 +1,32 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public enum GameModeEnumType: String, EnumType {
+  case none = "NONE"
+  case allPick = "ALL_PICK"
+  case captainsMode = "CAPTAINS_MODE"
+  case randomDraft = "RANDOM_DRAFT"
+  case singleDraft = "SINGLE_DRAFT"
+  case allRandom = "ALL_RANDOM"
+  case intro = "INTRO"
+  case theDiretide = "THE_DIRETIDE"
+  case reverseCaptainsMode = "REVERSE_CAPTAINS_MODE"
+  case theGreeviling = "THE_GREEVILING"
+  case tutorial = "TUTORIAL"
+  case midOnly = "MID_ONLY"
+  case leastPlayed = "LEAST_PLAYED"
+  case newPlayerPool = "NEW_PLAYER_POOL"
+  case compendiumMatchmaking = "COMPENDIUM_MATCHMAKING"
+  case custom = "CUSTOM"
+  case captainsDraft = "CAPTAINS_DRAFT"
+  case balancedDraft = "BALANCED_DRAFT"
+  case abilityDraft = "ABILITY_DRAFT"
+  case event = "EVENT"
+  case allRandomDeathMatch = "ALL_RANDOM_DEATH_MATCH"
+  case soloMid = "SOLO_MID"
+  case allPickRanked = "ALL_PICK_RANKED"
+  case turbo = "TURBO"
+  case mutation = "MUTATION"
+}
