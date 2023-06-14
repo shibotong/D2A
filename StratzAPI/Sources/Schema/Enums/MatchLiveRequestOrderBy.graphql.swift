ee05f2b85598ef17a83ae9b33de33af1917e807a
shibotong <shibo0617@gmail.com>
2023-06-14 21:46:36 +1000

    add match list view

---
+++ StratzAPI/Sources/Schema/Enums/MatchLiveRequestOrderBy.graphql.swift
@@ -0,0 +1,11 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public enum MatchLiveRequestOrderBy: String, EnumType {
+  case gameTime = "GAME_TIME"
+  case matchId = "MATCH_ID"
+  case spectatorCount = "SPECTATOR_COUNT"
+  case averageRank = "AVERAGE_RANK"
+}
