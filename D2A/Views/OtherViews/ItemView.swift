a2ac9ae4306569d0554fb3a38831efa321511d28
Shibo Tong <shibo0617@gmail.com>
2023-07-05 17:28:56 +1000

    Live match (#45)
    * add query for subscription
    
    * update live match
    
    * add live match timer view
    
    * add map view
    
    * add hero icon on map
    
    * add live match history query
    
    * update graph
    
    * update building status
    
    * update model
    
    * Update LiveMatchViewModel.swift
    
    * update tower event detail
    
    * relocate files
    
    * add kill event
    
    * update preview
    
    * add sample Item
    
    * Update ReadData.swift
    
    * add event view
    
    * update model
    
    * add events
    
    * events kill finish
    
    * add inventory history
    
    * update UI
    
    * update draft view
    
    * add draft view
    
    * add iPad view
    
    * migrate player row view to mvvm
    
    * update live match player
    
    * print win rate
    
    * update views
    
    * update UI
    
    * add match list view
    
    * update draft UI
    
    * update kill events
    
    * update ver
    
    * update debug print
    
    * fix hero icon not showing issue
    
    * update ui
    
    * update UI
    
    * add dynamic island
    
    * upda UI
    
    * update live activity
    
    * update live activity
    
    * update ui
    
    * update image
    
    * updateing position
    
    * update UI
    
    * updateUI
    
    * update ui
    
    * update UI
    
    * update itemview for refresh item
    
    * update player row view
    
    * update analysis view
    
    * update live match view
    
    * update localization
    
    * Update Localizable.strings
    
    * remove activity
    
    * add empty view
    
    * update some async func
    
    * fix cache issue
    
    * Update LiveMatchViewModel.swift

--- D2A/Views/OtherViews/ItemView.swift
+++ D2A/Views/OtherViews/ItemView.swift
@@ -34,6 +34,12 @@ struct ItemView: View {
         }
     }
     
+    private func startLoadingImage() {
+        Task {
+            await fetchImage()
+        }
+    }
+    
     private func computeURL() -> URL? {
         guard let id, let item = HeroDatabase.shared.fetchItem(id: id) else {
             return nil
@@ -43,22 +49,20 @@ struct ItemView: View {
     }
     
     private func fetchImage() async {
-        guard let id else {
-            setImage(nil)
+        if let id, let cacheImage = ImageCache.readImage(type: .item, id: id.description) {
+            DispatchQueue.main.async {
+                image = cacheImage
+            }
             return
         }
         
-        if let cacheImage = ImageCache.readImage(type: .item, id: id.description) {
-            setImage(cacheImage)
-            return
-        }
-        
-        guard let newImage = await loadImage() else {
-            setImage(nil)
+        guard let newImage = await loadImage(), let id else {
             return
         }
         ImageCache.saveImage(newImage, type: .item, id: id.description)
-        setImage(newImage)
+        DispatchQueue.main.async {
+            image = newImage
+        }
     }
     
     private func loadImage() async -> UIImage? {
@@ -68,11 +72,6 @@ struct ItemView: View {
             return nil
         }
         return newImage
-    }
-    
-    @MainActor
-    private func setImage(_ image: UIImage?) {
-        self.image = image
     }
 }
 
