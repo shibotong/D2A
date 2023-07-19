9614d450c35b34999d97a9cbd67da4b43832ac96
shibotong <shibo0617@gmail.com>
2023-07-19 20:56:43 +1000

    Update LiveMatchListView.swift

--- D2A/Views/LiveMatchView/LiveMatchListView.swift
+++ D2A/Views/LiveMatchView/LiveMatchListView.swift
@@ -17,45 +17,54 @@ struct LiveMatchListView: View {
     var body: some View {
         ScrollView {
             LazyVGrid(columns: gridItems) {
-                if viewModel.matches.isEmpty {
-                    ForEach(0...20, id: \.self) { _ in
-                        LiveMatchListRowEmptyView()
-                            .padding(5)
-                    }
-                } else {
-                    ForEach(viewModel.matches) { match in
-                        NavigationLink(destination: LiveMatchView(viewModel: LiveMatchViewModel(matchID: match.matchId.description))) {
-                            LiveMatchListRowView(radiantScore: match.radiantScore,
-                                                 direScore: match.direScore,
-                                                 radiantHeroes: match.radiantPlayers,
-                                                 direHeroes: match.direPlayers,
-                                                 radiantTeam: match.radiantTeam?.description,
-                                                 direTeam: match.direTeam?.description,
-                                                 averageRank: match.averageRank,
-                                                 leagueID: match.leagueId,
-                                                 leagueName: match.leagueName)
-                            .padding(5)
-                        }
-                    }
+                matchListView
+                if viewModel.loading {
+                    loadingView
                 }
+            }.padding()
+            if !viewModel.loading {
+                Button {
+                    viewModel.loadMore()
+                } label: {
+                    Text("Load more")
+                }
+                .buttonStyle(.bordered)
             }
-            .padding()
-            Button {
-                viewModel.loadMore()
-            } label: {
-                Text("Load more")
-            }
-            .buttonStyle(.bordered)
         }
         .refreshable {
             await viewModel.fetchMatchesAsync(existItems: 0)
         }
         .navigationTitle("Live")
     }
+    
+    private var matchListView: some View {
+        ForEach(viewModel.matches) { match in
+            NavigationLink(destination: LiveMatchView(viewModel: LiveMatchViewModel(matchID: match.matchId.description))) {
+                LiveMatchListRowView(radiantScore: match.radiantScore,
+                                     direScore: match.direScore,
+                                     radiantHeroes: match.radiantPlayers,
+                                     direHeroes: match.direPlayers,
+                                     radiantTeam: match.radiantTeam?.description,
+                                     direTeam: match.direTeam?.description,
+                                     averageRank: match.averageRank,
+                                     leagueID: match.leagueId,
+                                     leagueName: match.leagueName)
+                .padding(5)
+            }
+        }
+    }
+    
+    private var loadingView: some View {
+        ForEach(0...10, id: \.self) { _ in
+            LiveMatchListRowEmptyView()
+                .padding(5)
+        }
+    }
 }
 
 class LiveMatchListViewModel: ObservableObject {
     @Published var matches: [LiveMatch] = []
+    @Published var loading: Bool = false
     
     struct LiveMatch: Identifiable {
         var id: Int {
@@ -75,24 +84,33 @@ class LiveMatchListViewModel: ObservableObject {
     }
     
     init() {
-        self.fetchMatches(existItems: 0)
+        Task {
+            await fetchMatchesAsync(existItems: 0)
+        }
     }
     
     func loadMore() {
         let currentItems = matches.count
-        fetchMatches(existItems: currentItems)
+        Task {
+            await fetchMatchesAsync(existItems: currentItems)
+        }
     }
     
     func fetchMatchesAsync(existItems: Int) async {
+        if existItems == 0 {
+            await setMatches([], addMatches: false)
+        }
+        await setLoading(true)
         await withCheckedContinuation({ checked in
             fetchMatches(existItems: existItems) {
                 print("completion")
                 checked.resume()
             }
         })
+        await setLoading(false)
     }
     
-    func fetchMatches(existItems: Int, completion: (() -> Void)? = nil) {
+    private func fetchMatches(existItems: Int, completion: (() -> Void)? = nil) {
         let fetchQuery = MatchLiveRequestType(
             gameStates: [
                 .case(.teamShowcase),
@@ -106,7 +124,7 @@ class LiveMatchListViewModel: ObservableObject {
         Network.shared.apollo.fetch(query: LiveMatchListQuery(request: .init(fetchQuery)), cachePolicy: .fetchIgnoringCacheCompletely) { [weak self] result in
             switch result {
             case .success(let graphQLResult):
-                guard let matches = graphQLResult.data?.live?.matches, let self else {
+                guard let matches = graphQLResult.data?.live?.matches else {
                     return
                 }
                 let newMatches: [LiveMatch] = matches.map { matchData in
@@ -146,17 +164,29 @@ class LiveMatchListViewModel: ObservableObject {
                         direPlayers: direPlayers)
                     return match
                 }
-                if existItems == 0 {
-                    self.matches = []
-                }
-                for match in newMatches where !self.matches.contains(where: { $0.matchId == match.matchId }) {
-                    self.matches.append(match)
+                Task { [weak self] in
+                    await self?.setMatches(newMatches, addMatches: existItems != 0)
                 }
                 completion?()
             case .failure(let error):
                 print(error.localizedDescription)
                 completion?()
             }
+        }
+    }
+    
+    @MainActor
+    private func setLoading(_ loading: Bool) {
+        self.loading = loading
+    }
+    
+    @MainActor
+    private func setMatches(_ newMatches: [LiveMatch], addMatches: Bool) {
+        if !addMatches {
+            matches = []
+        }
+        for match in newMatches where !self.matches.contains(where: { $0.matchId == match.matchId }) {
+            self.matches.append(match)
         }
     }
 }
