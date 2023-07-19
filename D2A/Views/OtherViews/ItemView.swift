8533f50b2770476a9a2fb7eee5216c595a314f2a
shibotong <shibo0617@gmail.com>
2023-07-19 20:37:41 +1000

    update itemview to handle nil item

--- D2A/Views/OtherViews/ItemView.swift
+++ D2A/Views/OtherViews/ItemView.swift
@@ -34,12 +34,6 @@ struct ItemView: View {
         }
     }
     
-    private func startLoadingImage() {
-        Task {
-            await fetchImage()
-        }
-    }
-    
     private func computeURL() -> URL? {
         guard let id, let item = HeroDatabase.shared.fetchItem(id: id) else {
             return nil
@@ -49,20 +43,22 @@ struct ItemView: View {
     }
     
     private func fetchImage() async {
-        if let id, let cacheImage = ImageCache.readImage(type: .item, id: id.description) {
-            DispatchQueue.main.async {
-                image = cacheImage
-            }
+        guard let id else {
+            setImage(nil)
             return
         }
         
-        guard let newImage = await loadImage(), let id else {
+        if let cacheImage = ImageCache.readImage(type: .item, id: id.description) {
+            setImage(cacheImage)
+            return
+        }
+        
+        guard let newImage = await loadImage() else {
+            setImage(nil)
             return
         }
         ImageCache.saveImage(newImage, type: .item, id: id.description)
-        DispatchQueue.main.async {
-            image = newImage
-        }
+        setImage(newImage)
     }
     
     private func loadImage() async -> UIImage? {
@@ -72,6 +68,11 @@ struct ItemView: View {
             return nil
         }
         return newImage
+    }
+    
+    @MainActor
+    private func setImage(_ image: UIImage?) {
+        self.image = image
     }
 }
 
