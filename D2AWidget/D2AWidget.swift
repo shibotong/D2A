fb2f6652bc82ba00dde003ee7c89a8423fff3ebc
shibotong <shibo0617@gmail.com>
2024-01-24 16:06:41 +1100

    update user entry

--- D2AWidget/D2AWidget.swift
+++ D2AWidget/D2AWidget.swift
@@ -31,6 +31,7 @@ struct RecentMatchesWidget: Widget {
         .configurationDisplayName("Recent Matches")
         .description("Your recent matches.")
         .supportedFamilies([.systemSmall, .systemMedium])
+        .containerBackgroundRemovable(true)
     }
 }
 
@@ -43,13 +44,22 @@ struct LatestMatchWidget: Widget {
         .configurationDisplayName("Latest Match")
         .description("Your latest match.")
         .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
+        .containerBackgroundRemovable(true)
         
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct D2AWidgetUserEntry: TimelineEntry {
     let date: Date
+    let user: D2AWidgetUser?
+}
+
+struct D2AWidgetUser {
+    let userID: String
+    let userName: String
+    let image: UIImage?
     let matches: [RecentMatch]
-    let user: UserProfile?
     let subscription: Bool
+    
+    static let sampleUser = D2AWidgetUser(userID: "1234567", userName: "Test User", image: UIImage(named: "profile") ?? nil, matches: [], subscription: true)
 }
