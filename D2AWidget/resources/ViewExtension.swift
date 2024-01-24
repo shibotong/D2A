fed9fd00dcf762bc696190cb4551e5381cef1465
shibotong <shibo0617@gmail.com>
2024-01-24 16:06:51 +1100

    add view extension for ios 17

---
+++ D2AWidget/resources/ViewExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ViewExtension.swift
+//  D2A
+//
+//  Created by Shibo Tong on 24/1/2024.
+//
+
+import SwiftUI
+
+extension View {
+    func widgetBackground(_ backgroundView: some View) -> some View {
+        if #available(iOS 17.0, *) {
+            return containerBackground(for: .widget) {
+                backgroundView
+            }
+        } else {
+            return background(backgroundView)
+        }
+    }
+}
