62431c4936b2994f65b655f88380c63808d131c7
shibotong <shibo0617@gmail.com>
2024-01-25 15:31:08 +1100

    Update D2AWidget.swift

--- D2AWidget/D2AWidget.swift
+++ D2AWidget/D2AWidget.swift
@@ -59,7 +59,7 @@ struct D2AWidgetUser {
     let userID: String
     let userName: String
     let image: UIImage?
-    let matches: [RecentMatch]
+    let matches: [D2AWidgetMatch]
     let isPlus: Bool
     let rank: Int?
     let leaderboard: Int?
@@ -67,7 +67,21 @@ struct D2AWidgetUser {
     static let sampleUser = D2AWidgetUser(userID: "1234567",
                                           userName: "Test User",
                                           image: UIImage(named: "profile") ?? nil,
-                                          matches: [],
+                                          matches: [
+                                            .init(matchID: "1", heroID: 1, win: true),
+                                            .init(matchID: "2", heroID: 2, win: false),
+                                            .init(matchID: "3", heroID: 1, win: true),
+                                            .init(matchID: "4", heroID: 2, win: false),
+                                            .init(matchID: "5", heroID: 1, win: true),
+                                            .init(matchID: "6", heroID: 2, win: false),
+                                            .init(matchID: "7", heroID: 1, win: true),
+                                            .init(matchID: "8", heroID: 2, win: false),
+                                            .init(matchID: "9", heroID: 1, win: true),
+                                            .init(matchID: "10", heroID: 2, win: false),
+                                            .init(matchID: "11", heroID: 1, win: true),
+                                            .init(matchID: "12", heroID: 2, win: false),
+                                            .init(matchID: "13", heroID: 1, win: true)
+                                          ],
                                           isPlus: true,
                                           rank: 11,
                                           leaderboard: nil)
@@ -77,13 +91,14 @@ struct D2AWidgetUser {
               let userName = profile.name else {
                   return nil
               }        
+        let widgetMatches = matches.map { D2AWidgetMatch($0) }
         self.init(userID: userID, userName: userName,
-                  image: image, matches: matches,
+                  image: image, matches: widgetMatches,
                   isPlus: profile.isPlus, rank: Int(profile.rank),
                   leaderboard: Int(profile.leaderboard))
     }
     
-    init(userID: String, userName: String, image: UIImage?, matches: [RecentMatch], isPlus: Bool, rank: Int?, leaderboard: Int?) {
+    init(userID: String, userName: String, image: UIImage?, matches: [D2AWidgetMatch], isPlus: Bool, rank: Int?, leaderboard: Int?) {
         self.userID = userID
         self.userName = userName
         self.image = image
@@ -91,5 +106,22 @@ struct D2AWidgetUser {
         self.isPlus = isPlus
         self.rank = rank
         self.leaderboard = leaderboard
+    }
+}
+
+struct D2AWidgetMatch: Identifiable {
+    let id: String
+    let heroID: Int
+    let win: Bool
+    
+    init(_ match: RecentMatch) {
+        
+        self.init(matchID: match.id ?? "0", heroID: Int(match.heroID), win: match.playerWin)
+    }
+    
+    init(matchID: String, heroID: Int, win: Bool) {
+        id = matchID
+        self.heroID = heroID
+        self.win = win
     }
 }
