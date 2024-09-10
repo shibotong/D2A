debf950724ca245dafc835507a17d9e48341255a
Shibo Tong <shibo0617@gmail.com>
2024-09-10 11:40:51 +1000

    Update Ability.swift

--- Shared/Model/CoreDataModel/Ability.swift
+++ Shared/Model/CoreDataModel/Ability.swift
@@ -18,25 +18,45 @@ extension Ability {
     typealias Localisation = LocaliseQuery.Data.Constants.Ability
     
     /// Fetch `Ability` with `id` in `CoreData`
-    static func fetchAbility(id: Int, context: NSManagedObjectContext = PersistenceController.shared.container.viewContext) -> Ability? {
-        let viewContext = PersistenceController.shared.container.viewContext
+    static func fetchAbility(id: Int, viewContext: NSManagedObjectContext) -> Ability? {
         let request = Ability.fetchRequest()
 
         request.predicate = NSPredicate(format: "abilityID == %f", Double(id))
+        request.fetchLimit = 1
+        do {
+            let results = try viewContext.fetch(request)
+            return results.first
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+    /// Fetch `Ability` with `name` in `CoreData`
+    static func fetchAbility(name: String, viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext) -> Ability? {
+        let request = Ability.fetchRequest()
+        request.predicate = NSPredicate(format: "name == %@", name)
         request.fetchLimit = 1
         let results = try? viewContext.fetch(request)
         return results?.first
     }
     
-    /// Fetch `Ability` with `name` in `CoreData`
-    static func fetchAbility(name: String, context: NSManagedObjectContext = PersistenceController.shared.container.viewContext) -> Ability? {
-        let viewContext = PersistenceController.shared.container.viewContext
-        let request = Ability.fetchRequest()
-
-        request.predicate = NSPredicate(format: "name == %@", name)
-        request.fetchLimit = 1
-        let results = try? viewContext.fetch(request)
-        return results?.first
+    static func fetchAbilities(names: [String], viewContext: NSManagedObjectContext) -> [Ability] {
+        let predicate = NSPredicate(format: "name IN %@", names)
+        return runQuery(viewContext: viewContext, predicate: predicate)
+    }
+    
+    static func runQuery(viewContext: NSManagedObjectContext,
+                         predicate: NSPredicate? = nil) -> [Ability] {
+        let fetchRequest = Ability.fetchRequest()
+        fetchRequest.predicate = predicate
+        do {
+            let results = try viewContext.fetch(fetchRequest)
+            return results
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
     }
     
     static func save(abilityID: Int,
@@ -45,24 +65,33 @@ extension Ability {
                      localisation: Localisation,
                      type: AbilityType,
                      context: NSManagedObjectContext) throws {
-        let abilityData = Ability.fetchAbility(id: abilityID) ?? Ability(context: context)
+        let abilityData = Ability.fetchAbility(id: abilityID, viewContext: context) ?? Ability(context: context)
+        abilityData.updateValues(abilityID: abilityID,
+                                 abilityName: abilityName,
+                                 ability: ability)
         
-        abilityData.dname = ability.dname
-//        abilityData.imageURL = nil
-        abilityData.behaviour = ability.behavior?.transformString()
-        abilityData.targetTeam = ability.targetTeam?.transformString()
-        abilityData.targetType = ability.targetType?.transformString()
-        abilityData.dmgType = ability.damageType?.transformString()
-        abilityData.mc = ability.manaCost?.transformString()
-        abilityData.cd = ability.coolDown?.transformString()
-        abilityData.bkbPierce = ability.bkbPierce?.transformString()
-        abilityData.dispellable = ability.dispellable?.transformString()
-        
-        // update attributes and localisation
-        abilityData.updateAttributes(ability.attributes ?? [], context: context)
         abilityData.updateLocalisation(localisation, type: type, context: context)
         
         try context.save()
+    }
+    
+    func updateValues(abilityID: Int,
+                      abilityName: String,
+                      ability: AbilityCodable) {
+        self.abilityID = Int32(abilityID)
+        self.name = abilityName
+        self.dname = ability.dname
+        //        abilityData.imageURL = nil
+        self.behaviour = ability.behavior?.transformString()
+        self.targetTeam = ability.targetTeam?.transformString()
+        self.targetType = ability.targetType?.transformString()
+        self.dmgType = ability.damageType?.transformString()
+        self.mc = ability.manaCost?.transformString()
+        self.cd = ability.coolDown?.transformString()
+        self.bkbPierce = ability.bkbPierce?.transformString()
+        self.dispellable = ability.dispellable?.transformString()
+        
+        self.attributes = ability.attributes?.compactMap { AbilityAttribute(attribute: $0) }
     }
     
     // Update localisation
@@ -70,14 +99,18 @@ extension Ability {
                                     type: AbilityType,
                                     context: NSManagedObjectContext) {
         let localisation = fetchLocalisation(language: languageCode, context: context)
-        localisation.descriptionAbility = localisationData.language?.displayName ?? ""
         localisation.lore = localisationData.language?.lore
+        localisation.displayName = localisationData.language?.displayName ?? ""
         
         switch type {
         case .scepter:
             localisation.descriptionScepter = localisationData.language?.aghanimDescription
+            localisation.descriptionShard = nil
+            localisation.descriptionAbility = nil
         case .shared:
             localisation.descriptionShard = localisationData.language?.shardDescription
+            localisation.descriptionScepter = nil
+            localisation.descriptionAbility = nil
         case .none:
             localisation.descriptionScepter = localisationData.language?.aghanimDescription
             localisation.descriptionShard = localisationData.language?.shardDescription
@@ -85,21 +118,20 @@ extension Ability {
         }
     }
     
-    private func updateAttributes(_ attributes: [AbilityCodableAttribute],
-                                  context: NSManagedObjectContext) {
-        self.attributes?.allObjects.forEach {
-            guard let object = $0 as? NSManagedObject else {
-                return
-            }
-            context.delete(object)
-        }
+    private func updateAttributes(_ attributes: [AbilityCodableAttribute]) {
         
-        let attributes: [AbilityAttribute] = attributes.map {
-            let attribute = AbilityAttribute(context: context)
-            attribute.update($0)
-            attribute.ability = self
-            return attribute
-        }
+//        if let savedAttributes = self.attributes {
+//            removeFromAttributes(savedAttributes)
+//        }
+//        
+//        let attributes: [AbilityAttribute] = attributes.map {
+//            let attribute = AbilityAttribute(context: context)
+//            attribute.update($0)
+//            attribute.ability = self
+//            return attribute
+//        }
+//        
+//        addToAttributes(NSSet(array: attributes))
     }
     
     private func fetchLocalisation(language: Language, context: NSManagedObjectContext) -> AbilityLocalisation {
@@ -107,7 +139,7 @@ extension Ability {
               let savedLocalisation = localisations.first(where: { $0.language == language.rawValue }) else {
             let newLocalisation = AbilityLocalisation(context: context)
             newLocalisation.language = language.rawValue
-            newLocalisation.ability = self
+            addToLocalisations(newLocalisation)
             return newLocalisation
         }
         
