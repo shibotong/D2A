a181e0d4392a3836d7038349e14deaf255b62a7b
Shibo Tong <shibo0617@gmail.com>
2024-11-09 19:26:05 +1100

    update secret file

--- Shared/GraphQL/Secret.swift
+++ Shared/GraphQL/Secret.swift
@@ -11,7 +11,7 @@ struct Secrets: Decodable {
     let stratzToken: String
     
     static func load() throws -> Self {
-        let secretsFileUrl = Bundle.main.url(forResource: "secrets", withExtension: "json")
+        let secretsFileUrl = Bundle.main.url(forResource: "d2a-secrets", withExtension: "json")
         
         guard let secretsFileUrl = secretsFileUrl, let secretsFileData = try? Data(contentsOf: secretsFileUrl) else {
             fatalError("No `secrets.json` file found. Make sure to duplicate `secrets.json.sample` and remove the `.sample` extension.")
