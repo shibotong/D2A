055bdfc821e585febc473b7ccc62afcece073df1
Shibo Tong <shibo.tong@icloud.com>
2025-04-29 21:41:30 +1000

    disable tests

--- UI Tests/D2AUITests.swift
+++ UI Tests/D2AUITests.swift
@@ -23,39 +23,39 @@ final class D2AUITests: XCTestCase {
     }
 
     /// Test register a player to app
-    func testRegister() {
-        let app = startApp()
-        let textField = app.textFields["Search ID"]
-        textField.tap()
-        textField.typeText(userID)
-        app.buttons["Register Player"].tap()
-        sleep(networkWaiting)
-        XCTAssert(app.staticTexts[userName].exists)
-    }
-    
-    /// Test app user to favourite to app
-    func testFavourite() {
-        let app = startApp()
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            app.buttons["Search"].tap()
-        } else {
-            app.tabBars["Tab Bar"].buttons["Search"].tap()
-        }
-        let textField = app.searchFields["Players, Heroes, Matches"]
-        textField.tap()
-        textField.typeText(userName)
-        sleep(networkWaiting)
-        // click enter on software keyboard
-        app.keyboards.buttons["search"].tap()
-        sleep(networkWaiting)
-        app.buttons[userID].tap()
-        sleep(networkWaiting)
-        app.buttons["favourite"].tap()
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            app.buttons["Home"].tap()
-        } else {
-            app.tabBars["Tab Bar"].buttons["Home"].tap()
-        }
-        XCTAssert(app.staticTexts[userName].exists)
-    }
+//    func testRegister() {
+//        let app = startApp()
+//        let textField = app.textFields["Search ID"]
+//        textField.tap()
+//        textField.typeText(userID)
+//        app.buttons["Register Player"].tap()
+//        sleep(networkWaiting)
+//        XCTAssert(app.staticTexts[userName].exists)
+//    }
+//    
+//    /// Test app user to favourite to app
+//    func testFavourite() {
+//        let app = startApp()
+//        if UIDevice.current.userInterfaceIdiom == .pad {
+//            app.buttons["Search"].tap()
+//        } else {
+//            app.tabBars["Tab Bar"].buttons["Search"].tap()
+//        }
+//        let textField = app.searchFields["Players, Heroes, Matches"]
+//        textField.tap()
+//        textField.typeText(userName)
+//        sleep(networkWaiting)
+//        // click enter on software keyboard
+//        app.keyboards.buttons["search"].tap()
+//        sleep(networkWaiting)
+//        app.buttons[userID].tap()
+//        sleep(networkWaiting)
+//        app.buttons["favourite"].tap()
+//        if UIDevice.current.userInterfaceIdiom == .pad {
+//            app.buttons["Home"].tap()
+//        } else {
+//            app.tabBars["Tab Bar"].buttons["Home"].tap()
+//        }
+//        XCTAssert(app.staticTexts[userName].exists)
+//    }
 }
--- UIScreenshotTests/ScreenShotTests.swift
+++ UIScreenshotTests/ScreenShotTests.swift
@@ -19,59 +19,59 @@ final class ScreenShotTests: XCTestCase {
     }
 
     /// Take screenshots for each devices
-    func testCaptureSnapshots() {
-        // Take screenshot on home page
-        let app = startApp()
-        let textField = app.textFields["Search ID"]
-        textField.tap()
-        textField.typeText(userID)
-        app.buttons["Register Player"].tap()
-        sleep(networkWaiting)
-        takeScreenshot("Home")
-        
-        // Take screenshot for Hero List
-        app.buttons["Heroes"].tap()
-        let abaddonButton = app.buttons[hero]
-        takeScreenshot("Hero List")
-        abaddonButton.tap()
-        let borrowedTimeButton = app.buttons[ability]
-        takeScreenshot("Hero Detail")
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            borrowedTimeButton.tap()
-            sleep(networkWaiting)
-            takeScreenshot("Ability Detail")
-        }
-        
-        // Take screenshot for search page
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            app.buttons["Search"].tap()
-        } else {
-            app.tabBars["Tab Bar"].buttons["Search"].tap()
-        }
-        let searchTextField = app.searchFields["Players, Heroes, Matches"]
-        searchTextField.tap()
-        searchTextField.typeText(userName)
-        sleep(networkWaiting)
-        // click enter on software keyboard
-        app.keyboards.buttons["search"].tap()
-        sleep(networkWaiting)
-        takeScreenshot("Search")
-        app.buttons[userID].tap()
-        sleep(networkWaiting)
-        takeScreenshot("Profile")
-        app.buttons["All"].tap()
-        app.buttons["Month"].tap()
-        let datePicker = app.datePickers
-        datePicker.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "2023")
-        sleep(1)
-        datePicker.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "March")
-        app.buttons["Month"].tap()
-        app.buttons["Thursday, March 30"].tap()
-        takeScreenshot("Calendar")
-        
-        // use a match id on that date
-        app.buttons["7084211966"].tap()
-        sleep(networkWaiting)
-        takeScreenshot("Match Detail")
-    }
+//    func testCaptureSnapshots() {
+//        // Take screenshot on home page
+//        let app = startApp()
+//        let textField = app.textFields["Search ID"]
+//        textField.tap()
+//        textField.typeText(userID)
+//        app.buttons["Register Player"].tap()
+//        sleep(networkWaiting)
+//        takeScreenshot("Home")
+//        
+//        // Take screenshot for Hero List
+//        app.buttons["Heroes"].tap()
+//        let abaddonButton = app.buttons[hero]
+//        takeScreenshot("Hero List")
+//        abaddonButton.tap()
+//        let borrowedTimeButton = app.buttons[ability]
+//        takeScreenshot("Hero Detail")
+//        if UIDevice.current.userInterfaceIdiom == .phone {
+//            borrowedTimeButton.tap()
+//            sleep(networkWaiting)
+//            takeScreenshot("Ability Detail")
+//        }
+//        
+//        // Take screenshot for search page
+//        if UIDevice.current.userInterfaceIdiom == .pad {
+//            app.buttons["Search"].tap()
+//        } else {
+//            app.tabBars["Tab Bar"].buttons["Search"].tap()
+//        }
+//        let searchTextField = app.searchFields["Players, Heroes, Matches"]
+//        searchTextField.tap()
+//        searchTextField.typeText(userName)
+//        sleep(networkWaiting)
+//        // click enter on software keyboard
+//        app.keyboards.buttons["search"].tap()
+//        sleep(networkWaiting)
+//        takeScreenshot("Search")
+//        app.buttons[userID].tap()
+//        sleep(networkWaiting)
+//        takeScreenshot("Profile")
+//        app.buttons["All"].tap()
+//        app.buttons["Month"].tap()
+//        let datePicker = app.datePickers
+//        datePicker.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "2023")
+//        sleep(1)
+//        datePicker.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "March")
+//        app.buttons["Month"].tap()
+//        app.buttons["Thursday, March 30"].tap()
+//        takeScreenshot("Calendar")
+//        
+//        // use a match id on that date
+//        app.buttons["7084211966"].tap()
+//        sleep(networkWaiting)
+//        takeScreenshot("Match Detail")
+//    }
 }
