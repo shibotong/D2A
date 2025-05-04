b4f1a253b4d73ad2ab41e92bdbd090ac2502c4e8
Shibo Tong <shibo.tong@icloud.com>
2025-05-04 14:58:28 +1000

    refactor odota hero loading pattern

--- Shared/Backend/OpenDota/OpenDotaConstantProvider.swift
+++ Shared/Backend/OpenDota/OpenDotaConstantProvider.swift
@@ -10,7 +10,7 @@ protocol OpenDotaConstantProviding {
     func loadHeroes() async -> [String: ODHero]
     func loadItemIDs() async -> [String: String]
     func loadAbilities() async -> [ODAbility]
-    func loadAbilitiesForHeroes() async -> [String: [Int]]
+    func loadAbilitiesForHeroes() async -> [String: ODHeroAbilities]
 }
 
 class OpenDotaConstantProvider: OpenDotaConstantProviding {
@@ -20,58 +20,60 @@ class OpenDotaConstantProvider: OpenDotaConstantProviding {
     private let maxHeroID = 150
     
     func loadHeroes() async -> [String: ODHero] {
-        let heroURL = OpenDotaConstantService.heroes.serviceURL
-        do {
-            let heroes = try await D2ANetwork.default.dataTask(heroURL, as: [String: ODHero].self)
-
-            return heroes
-        } catch {
-            logWarn("Loading heroes from OpenDotaConstantController failed: \(error.localizedDescription)", category: .opendotaConstant)
+        guard let heroDict = await loadOpenDotaConstantService(service: .heroes, type: [String: ODHero].self) else {
             return [:]
         }
+        return heroDict
     }
     
     func loadAbilities() async -> [ODAbility] {
-        let abilityURL = OpenDotaConstantService.abilities.serviceURL
-        let abilityIDURL = OpenDotaConstantService.abilityIDs.serviceURL
-        
-        do {
-            let abilityDict = try await D2ANetwork.default.dataTask(abilityURL, as: [String: ODAbility].self)
-            let abilityIDs = try await D2ANetwork.default.dataTask(abilityIDURL, as: [String: String].self)
-            
-            var abilities: [ODAbility] = []
-            
-            for (abilityIDString, name) in abilityIDs {
-                guard var ability = abilityDict[name] else {
-                    logDebug("\(name) cannot be found", category: .opendotaConstant)
-                    continue
-                }
-                guard let abilityID = Int(abilityIDString) else {
-                    logWarn("\(abilityIDString) abilityID is not a number", category: .opendotaConstant)
-                    continue
-                }
-                guard let dname = ability.dname, !dname.isEmpty else {
-                    continue
-                }
-                ability.id = abilityID
-                ability.name = name
-                abilities.append(ability)
-            }
-            return abilities
-        } catch {
-            logWarn("Loading abilities from OpenDotaConstantController failed: \(error.localizedDescription)", category: .opendotaConstant)
+        guard let abilityDict = await loadOpenDotaConstantService(service: .abilities, type: [String: ODAbility].self),
+              let abilityIDs = await loadOpenDotaConstantService(service: .abilityIDs, type: [String: String].self) else {
             return []
         }
+        var abilities: [ODAbility] = []
+        
+        for (abilityIDString, name) in abilityIDs {
+            guard var ability = abilityDict[name] else {
+                logDebug("\(name) cannot be found", category: .opendotaConstant)
+                continue
+            }
+            guard let abilityID = Int(abilityIDString) else {
+                logWarn("\(abilityIDString) abilityID is not a number", category: .opendotaConstant)
+                continue
+            }
+            guard let dname = ability.dname, !dname.isEmpty else {
+                continue
+            }
+            ability.id = abilityID
+            ability.name = name
+            abilities.append(ability)
+        }
+        return abilities
     }
     
     func loadItemIDs() async -> [String: String] {
-        let itemIDURL = OpenDotaConstantService.itemIDs.serviceURL
-        do {
-            let itemIDs = try await D2ANetwork.default.dataTask(itemIDURL, as: [String: String].self)
-            return itemIDs
-        } catch {
-            logWarn("Loading itemIDs from OpenDotaConstantController failed: \(error.localizedDescription)", category: .opendotaConstant)
+        guard let itemIDs = await loadOpenDotaConstantService(service: .itemIDs, type: [String: String].self) else {
             return [:]
+        }
+        return itemIDs
+    }
+    
+    func loadAbilitiesForHeroes() async -> [String: ODHeroAbilities] {
+        guard let heroAbilities = await loadOpenDotaConstantService(service: .heroAbilities, type: [String: ODHeroAbilities].self) else {
+            return [:]
+        }
+        return heroAbilities
+    }
+    
+    private func loadOpenDotaConstantService<T: Decodable>(service: OpenDotaConstantService, type: T.Type) async -> T? {
+        let url = service.serviceURL
+        do {
+            let data = try await D2ANetwork.default.dataTask(url, as: T.self)
+            return data
+        } catch {
+            logWarn("Loading \(service) from OpenDota failed: \(error)", category: .opendotaConstant)
+            return nil
         }
     }
 
