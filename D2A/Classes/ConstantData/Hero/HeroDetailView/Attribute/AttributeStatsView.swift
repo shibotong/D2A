e6f316fcbad1bdfff91d43055f2997fd94f1ac78
Shibo Tong <shibo.tong@icloud.com>
2025-05-07 21:15:58 +1000

    extract attribute section

---
+++ D2A/Classes/ConstantData/Hero/HeroDetailView/Attribute/AttributeStatsView.swift
@@ -0,0 +1,44 @@
+//
+//  AttributeStatsView.swift
+//  D2A
+//
+//  Created by Shibo Tong on 7/5/2025.
+//
+
+import SwiftUI
+
+struct AttributeStatsView: View {
+    
+    let level: Int
+    let hero: Hero
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            buildStatLevel(type: .str)
+            Spacer()
+            buildStatLevel(type: .agi)
+            Spacer()
+            buildStatLevel(type: .int)
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    private func buildStatLevel(type: HeroAttribute) -> some View {
+        HStack {
+            let gain = hero.getGain(type: type)
+            AttributeImage(attribute: type)
+                .frame(width: 15, height: 15)
+            Text("\(hero.calculateAttribute(level: Double(level), attr: type))")
+                .font(.system(size: 18))
+                .bold()
+            Text("+ \(gain, specifier: "%.1f")")
+                .font(.system(size: 13))
+        }
+    }
+}
+
+#Preview {
+    AttributeStatsView(level: 1, hero: Hero.previewHeroes.first!)
+}
---
+++ D2A/Classes/ConstantData/Hero/HeroDetailView/SectionTitle.swift
@@ -0,0 +1,24 @@
+//
+//  SectionTitle.swift
+//  D2A
+//
+//  Created by Shibo Tong on 7/5/2025.
+//
+
+import SwiftUI
+
+struct SectionTitle: View {
+    
+    let title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.system(size: 15))
+                .bold()
+            Spacer()
+        }.padding(.bottom)
+    }
+}
+
+
