9cf374e9f43fcb94d37e7deaabcfd1905cb66f03
Shibo Tong <shibo0617@gmail.com>
2025-07-01 23:27:22 +1000

    Refactor image caching to use ImageProvider
    Moved image loading and saving logic from ImageCache to ImageProvider, consolidating image management. Updated NetworkImage to use dependency-injected ImageProvider. Added new D2AServiceCategory case for image logging.

--- D2AWidget/resources/NetworkImage.swift
+++ D2AWidget/resources/NetworkImage.swift
@@ -14,6 +14,8 @@ struct NetworkImage: View {
 
     let imageType: ImageCacheType
     let isRadiant: Bool
+    
+    private let imageProvider: ImageProviding
 
     private var emptyImage: String {
         switch imageType {
@@ -32,30 +34,34 @@ struct NetworkImage: View {
         }
     }
 
-    init(profile: UserProfile) {
+    init(profile: UserProfile,
+         imageProvider: ImageProviding = ImageProvider.shared) {
         userID = profile.id!
         urlString = profile.avatarfull!
         imageType = .avatar
         isRadiant = true
-        image = ImageCache.readImage(type: .avatar, id: userID)
+        self.imageProvider = imageProvider
+        image = imageProvider.localImage(type: .avatar, id: userID, fileExtension: .jpg)
     }
 
-    init(userID: String, urlString: String, image: UIImage) {
+    init(userID: String, urlString: String, image: UIImage, imageProvider: ImageProviding = ImageProvider.shared) {
         self.userID = userID
         self.urlString = urlString
         imageType = .avatar
         isRadiant = true
+        self.imageProvider = imageProvider
         self.image = image
     }
 
-    init(teamID: String, isRadiant: Bool) {
+    init(teamID: String, isRadiant: Bool, imageProvider: ImageProviding = ImageProvider.shared) {
         self.urlString = "https://cdn.stratz.com/images/dota2/teams/\(teamID).png"
         self.userID = teamID
         self.isRadiant = isRadiant
         imageType = .teamIcon
+        self.imageProvider = imageProvider
         print(teamID)
         if !teamID.isEmpty {
-            image = ImageCache.readImage(type: .teamIcon, id: teamID, fileExtension: "png")
+            image = imageProvider.localImage(type: .teamIcon, id: teamID, fileExtension: .png)
         } else {
             image = nil
         }
