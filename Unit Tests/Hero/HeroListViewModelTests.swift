dc7187fa28c04e5bbf4c758909f58a37118ead7d
Shibo Tong <shibo0617@gmail.com>
2025-07-01 18:46:55 +1000

    Expand attribute search tests in HeroListViewModelTests
    Added additional assertions to test searching heroes by strength, intelligence, and universal attributes, improving test coverage for attribute-based filtering.

--- Unit Tests/Hero/HeroListViewModelTests.swift
+++ Unit Tests/Hero/HeroListViewModelTests.swift
@@ -31,6 +31,12 @@ final class HeroListViewModelTests: XCTestCase {
     func testSearchHeroesByAttribute() {
         viewModel.selectedAttribute = .agi
         XCTAssertEqual(viewModel.searchedResults.count, 5, "Only 5 heroes are agility")
+        viewModel.selectedAttribute = .str
+        XCTAssertEqual(viewModel.searchedResults.count, 2, "Only 2 heroes are strength")
+        viewModel.selectedAttribute = .int
+        XCTAssertEqual(viewModel.searchedResults.count, 1, "Only 1 heroes are intellegence")
+        viewModel.selectedAttribute = .uni
+        XCTAssertEqual(viewModel.searchedResults.count, 2, "Only 2 heroes are universal")
     }
 
 }
