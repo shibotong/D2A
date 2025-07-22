10f0a9f8e002bd9d8988fb60aef0947a816dc1f8
Shibo Tong <shibo0617@gmail.com>
2025-07-22 18:11:25 +1000

    revert changes

--- Shared/Backend/OpenDota/Constants/OpenDotaConstantService.swift
+++ Shared/Backend/OpenDota/Constants/OpenDotaConstantService.swift
@@ -8,26 +8,40 @@
 import Foundation
 
 enum OpenDotaConstantService: String, CaseIterable {
-
-    static let baseURL = "https://raw.githubusercontent.com/odota/dotaconstants/master/build"
-
-    case abilities
-    case abilityIDs = "ability_ids"
-    case aghsDesc = "aghs_desc"
-    case countries
-    case gameMode = "game_mode"
-    case heroAbilities = "hero_abilities"
-    case heroLore = "hero_lore"
-    case heroes
     
-    case itemIDs = "item_ids"
+    static let baseURL = "https://raw.githubusercontent.com/odota/dotaconstants/master/build"
+    
+    case heroes
+    case abilities
+    case abilityIDs
+    case heroAbilities
+    case aghs
     case items
-    case lobbyType = "lobby_type"
-    case permanentBuffs = "permanent_buffs"
-    case region
-    case xpLevel = "xp_level"
+    case itemIDs
+    case gameModes
     
     var serviceURL: String {
-        "\(Self.baseURL)/\(rawValue).json"
+        "\(Self.baseURL)/\(name).json"
+    }
+    
+    private var name: String {
+        switch self {
+        case .heroes:
+            "heroes"
+        case .abilities:
+            "abilities"
+        case .abilityIDs:
+            "ability_ids"
+        case .heroAbilities:
+            "hero_abilities"
+        case .aghs:
+            "aghs_desc"
+        case .items:
+            "items"
+        case .itemIDs:
+            "item_ids"
+        case .gameModes:
+            "game_mode"
+        }
     }
 }
