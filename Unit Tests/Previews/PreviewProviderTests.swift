cd05eed0ae74c1f36465786f28c9772d45beb9bf
Shibo Tong <shibo0617@gmail.com>
2025-07-23 17:07:42 +1000

    create hero abilities data

--- Unit Tests/Previews/PreviewProviderTests.swift
+++ Unit Tests/Previews/PreviewProviderTests.swift
@@ -23,3 +23,33 @@ func testPreviewProvider() {
 func loadService<T: Decodable>(service: OpenDotaConstantService, as type: T.Type) -> T? {
     return provider.loadOpenDotaConstants(service: service, as: type)
 }
+
+
+@Suite("PreviewDataProvider tests")
+struct PreviewProviderTests {
+    let provider = PreviewDataProvider()
+    
+    @Test("abilities")
+    func abilities() {
+        let abilities = loadService(service: .abilities, as: [String: ODAbility].self) ?? [:]
+        #expect(abilities.count > 0, "abilities should not be empty")
+    }
+    
+    @Test("abilityIDs")
+    func abilityIDs() {
+        let abilityIDs = loadService(service: .abilityIDs, as: [String: String].self) ?? [:]
+        #expect(abilityIDs.count > 0, "ability IDs should not be empty")
+    }
+    
+    @Test("heroes")
+    func heroes() {
+        let heroes = loadService(service: .heroes, as: [String: ODHero].self) ?? [:]
+        #expect(heroes.count > 0, "heroes should not be empty")
+    }
+    
+    @Test("hero abilities")
+    func heroAbilities() {
+        let heroAbilities = loadService(service: .heroAbilities, as: [String: ODHeroAbilities].self) ?? [:]
+        #expect(heroAbilities.count > 0, "hero abilities should not be empty")
+    }
+}
