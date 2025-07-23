7463adf2a2623ad0cf1a4f635f5248bc9ef4241f
Shibo Tong <shibo0617@gmail.com>
2025-07-23 17:29:08 +1000

    Update PreviewProviderTests.swift

--- Unit Tests/Previews/PreviewProviderTests.swift
+++ Unit Tests/Previews/PreviewProviderTests.swift
@@ -8,23 +8,6 @@
 import Testing
 @testable import D2A
 
-let provider = PreviewDataProvider()
-
-@Test
-func testPreviewProvider() {
-    let abilities = loadService(service: .abilities, as: [String: ODAbility].self)
-    #expect(abilities?.count != 0, "abilities should not be empty")
-    let abilityIDs = loadService(service: .abilityIDs, as: [String: String].self)
-    #expect(abilityIDs?.count != 0, "ability IDs should not be empty")
-    let heroes = loadService(service: .heroes, as: [String: ODHero].self)
-    #expect(heroes?.count != 0, "heroes should not be empty")
-    
-    let heroLore = loadService(service: .heroLore, as: [String: String].self)
-    #expect(heroLore?.count != 0, "hero lore should not be empty")
-}
-
-func loadService<T: Decodable>(service: OpenDotaConstantService, as type: T.Type) -> T? {
-    return provider.loadOpenDotaConstants(service: service, as: type)
 @Suite("PreviewDataProvider tests")
 struct PreviewProviderTests {
     let provider = PreviewDataProvider()
