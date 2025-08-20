b785b9e0876ca4268c2c0d499c87143cf01bbd9f
Shibo Tong <shibo0617@gmail.com>
2025-08-20 22:31:26 +1000

    update localisation

--- D2A-iOS/Classes/Search/SearchHistoryView.swift
+++ D2A-iOS/Classes/Search/SearchHistoryView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct SearchHistoryView: View {
     @Environment(\.managedObjectContext) var context
@@ -13,18 +14,67 @@ struct SearchHistoryView: View {
     @FetchRequest(sortDescriptors: [SortDescriptor(\.searchTime)])
     var searchHistories: FetchedResults<SearchHistory>
     
+    @State var clearSearchDialogIsPresented = false
+    
     var body: some View {
-        ForEach(searchHistories) { history in
-            if let hero = history.hero {
-                NavigationLink(destination: HeroDetailViewV2(hero: hero)) {
-                    SearchHeroRowView(hero: hero)
+        ScrollView {
+            VStack {
+                HStack {
+                    Text(LocalizableStrings.recentlySearched)
+                        .bold()
+                    Spacer()
+                    Button {
+                        clearSearchDialogIsPresented = true
+                    } label: {
+                        Text(LocalizableStrings.clear)
+                            .bold()
+                    }
+                }
+                Divider()
+                ForEach(searchHistories) { history in
+                    if let hero = history.hero {
+                        NavigationLink(destination: HeroDetailViewV2(hero: hero)) {
+                            HStack {
+                                SearchHeroRowView(hero: hero)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .bold()
+                            }
+                        }
+                    }
+                    if let user = history.player {
+                        NavigationLink(destination: PlayerProfileView(userid: user.id ?? "")) {
+                            HStack {
+                                ProfileView(viewModel: ProfileViewModel(profile: user))
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .bold()
+                            }
+                        }
+                    }
+                    Divider()
                 }
             }
-            if let user = history.player {
-                NavigationLink(destination: PlayerProfileView(userid: user.id ?? "")) {
-                    ProfileView(viewModel: ProfileViewModel(profile: user))
-                }
+            .padding(.horizontal)
+        }
+        .confirmationDialog(LocalizableStrings.clearSearchTitle, isPresented: $clearSearchDialogIsPresented, titleVisibility: .visible) {
+            Button(LocalizableStrings.clearSearchButton, role: .destructive, action: clearSearch)
+            Button(LocalizableStrings.cancel, role: .cancel) {
+                clearSearchDialogIsPresented = false
             }
+        } message: {
+            Text(LocalizableStrings.clearSearchDescription)
+        }
+    }
+    
+    private func clearSearch() {
+        for search in searchHistories {
+            context.delete(search)
+        }
+        do {
+            try context.save()
+        } catch {
+            logError("Failed to batch delete search histories: \(error)", category: .coredata)
         }
     }
 }
--- D2A-iOS/LocalizableStrings.swift
+++ D2A-iOS/LocalizableStrings.swift
@@ -6,10 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct LocalizableStrings {
     static let kills = NSLocalizedString("Kills", comment: "")
     static let towerDamage = NSLocalizedString("Tower Damage", comment: "")
     static let heroDamage = NSLocalizedString("Hero Damage", comment: "")
     static let gold = NSLocalizedString("Net Worth", comment: "")
+    
+    static let recentlySearched = LocalizedStringKey("RECENT_SEARCH")
+    static let clear = LocalizedStringKey("CLEAR")
+    static let clearSearchTitle = LocalizedStringKey("CLEARSEARCHTITLE")
+    static let clearSearchDescription = LocalizedStringKey("CLEARSEARCHDESCRIPTION")
+    static let clearSearchButton = LocalizedStringKey("CLEARSEARCHBUTTON")
+    
+    static let cancel = LocalizedStringKey("CANCEL")
 }
