f3beb42bff44b007a6a34697a7214c88034322e8
Shibo Tong <shibo.tong@icloud.com>
2025-09-25 20:10:50 +1000

    Create ODPatch.swift

---
+++ Shared/Backend/OpenDota/Constants/Models/ODPatch.swift
@@ -0,0 +1,32 @@
+//
+//  ODPatch.swift
+//  D2A
+//
+//  Created by Shibo Tong on 25/9/2025.
+//
+
+import Foundation
+
+struct ODPatch: Decodable {
+    let name: String
+    let id: Int
+    let date: String
+    
+    var dictionary: [String: Any] {
+        let formatter = ISO8601DateFormatter()
+        let date = {
+            if let date = formatter.date(from: date) {
+                return date
+            } else {
+                logError("Not able to parse date string: \(self.date)", category: .opendota)
+                return Date()
+            }
+        }()
+        
+        return [
+            "name": name,
+            "patchID": Int16(id),
+            "date": date
+        ]
+    }
+}
