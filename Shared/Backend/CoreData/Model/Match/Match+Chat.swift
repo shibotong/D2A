b7bbe6996a705836fd169dcee3bd7be4ae60710f
Shibo Tong <shibo.tong@icloud.com>
2025-09-30 18:48:21 +1000

    Update Match+Chat.swift

--- Shared/Backend/CoreData/Model/Match/Match+Chat.swift
+++ Shared/Backend/CoreData/Model/Match/Match+Chat.swift
@@ -13,22 +13,22 @@ public class Chat: NSObject, NSSecureCoding {
     public static let supportsSecureCoding: Bool = true
     
     public var time: Int
-    public var unit: String
+    public var type: String
     public var key: String
     public var slot: Int
     public var playerSlot: Int?
     
     enum CodingKeys: String, CodingKey {
         case time
-        case unit
+        case type
         case key
         case slot
         case playerSlot = "player_slot"
     }
     
-    init(time: Int, unit: String, key: String, slot: Int, playerSlot: Int? = nil) {
+    init(time: Int, type: String, key: String, slot: Int, playerSlot: Int? = nil) {
         self.time = time
-        self.unit = unit
+        self.type = type
         self.key = key
         self.slot = slot
         self.playerSlot = playerSlot
@@ -36,7 +36,7 @@ public class Chat: NSObject, NSSecureCoding {
     
     convenience init?(from json: [String: Any]) {
         guard let time = json[CodingKeys.time.rawValue] as? Int,
-              let unit = json[CodingKeys.unit.rawValue] as? String,
+              let type = json[CodingKeys.type.rawValue] as? String,
               let key = json[CodingKeys.key.rawValue] as? String,
               let slot = json[CodingKeys.slot.rawValue] as? Int else {
             logError("Failed to decode Chat from json.", category: .coredata)
@@ -44,11 +44,11 @@ public class Chat: NSObject, NSSecureCoding {
         }
         
         let playerSlot = json[CodingKeys.playerSlot.rawValue] as? Int
-        self.init(time: time, unit: unit, key: key, slot: slot, playerSlot: playerSlot)
+        self.init(time: time, type: type, key: key, slot: slot, playerSlot: playerSlot)
     }
     
     public required convenience init?(coder: NSCoder) {
-        guard let unit = coder.decodeObject(forKey: CodingKeys.unit.rawValue) as? String,
+        guard let type = coder.decodeObject(forKey: CodingKeys.type.rawValue) as? String,
                 let key = coder.decodeObject(forKey: CodingKeys.key.rawValue) as? String else {
             logError("Failed to decode Chat", category: .coredata)
             return nil
@@ -58,12 +58,12 @@ public class Chat: NSObject, NSSecureCoding {
         let slot = coder.decodeInteger(forKey: CodingKeys.slot.rawValue)
         let playerSlot = coder.decodeObject(forKey: CodingKeys.playerSlot.rawValue) as? Int
         
-        self.init(time: time, unit: unit, key: key, slot: slot, playerSlot: playerSlot)
+        self.init(time: time, type: type, key: key, slot: slot, playerSlot: playerSlot)
     }
     
     public func encode(with coder: NSCoder) {
         coder.encode(time, forKey: CodingKeys.time.rawValue)
-        coder.encode(unit, forKey: CodingKeys.unit.rawValue)
+        coder.encode(type, forKey: CodingKeys.type.rawValue)
         coder.encode(key, forKey: CodingKeys.key.rawValue)
         coder.encode(slot, forKey: CodingKeys.slot.rawValue)
         coder.encode(playerSlot, forKey: CodingKeys.playerSlot.rawValue)
