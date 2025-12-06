33950fe7ffa2449ae8ebfc7782dba9e70751d1d6
Shibo Tong <shibo.tong@icloud.com>
2025-12-06 13:05:39 +1100

    create network data

--- Shared/Backend/D2ANetwork.swift
+++ Shared/Backend/D2ANetwork.swift
@@ -15,15 +15,27 @@ protocol D2ANetworking {
 
 struct D2ANetwork: D2ANetworking {
     func jsonObject(urlString: String) async throws -> [String : Any] {
-        <#code#>
+        let data = try await data(urlString: urlString)
+        guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+            throw URLError(.cannotDecodeRawData)
+        }
+        return jsonObject
     }
     
     func jsonArray(urlString: String) async throws -> [[String : Any]] {
-        <#code#>
+        let data = try await data(urlString: urlString)
+        guard let jsonArray = try JSONSerialization.jsonObject(with: data) as? [[String: Any]] else {
+            throw URLError(.cannotDecodeRawData)
+        }
+        return jsonArray
     }
     
     func image(urlString: String) async throws -> UIImage {
-        <#code#>
+        let data = try await data(urlString: urlString)
+        guard let image = UIImage(data: data) else {
+            throw URLError(.cannotDecodeRawData)
+        }
+        return image
     }
     
     private func data(urlString: String) async throws -> Data {
@@ -31,6 +43,11 @@ struct D2ANetwork: D2ANetworking {
             throw URLError(.badURL)
         }
         let (data, response) = try await URLSession.shared.data(from: url)
-        if response
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return data
     }
 }
