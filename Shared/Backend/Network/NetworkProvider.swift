51ac6cbd3644c23fd638c6977ddda95517e62c70
Shibo Tong <shibo.tong@icloud.com>
2025-12-06 15:35:26 +1100

    Create d2a network (#175)
    * move files
    
    * create network data
    
    * create a data provider for unit tests
    
    * create networking
    
    * rename file

---
+++ Shared/Backend/Network/NetworkProvider.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkProvider.swift
+//  D2A
+//
+//  Created by Shibo Tong on 6/12/2025.
+//
+
+import UIKit
+
+protocol NetworkProviding {
+    func jsonObject(urlString: String) async throws -> [String: Any]
+    func jsonArray(urlString: String) async throws -> [[String: Any]]
+    func image(urlString: String) async throws -> UIImage
+}
+
+struct NetworkProvider: NetworkProviding {
+    
+    private let provider: DataProviding
+    
+    init(provider: DataProviding = DataProvider()) {
+        self.provider = provider
+    }
+    
+    func jsonObject(urlString: String) async throws -> [String : Any] {
+        let data = try await provider.data(urlString: urlString)
+        guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+            logger.error("Cannot decode as json object from \(urlString)")
+            throw URLError(.cannotDecodeRawData)
+        }
+        return jsonObject
+    }
+    
+    func jsonArray(urlString: String) async throws -> [[String : Any]] {
+        let data = try await provider.data(urlString: urlString)
+        guard let jsonArray = try JSONSerialization.jsonObject(with: data) as? [[String: Any]] else {
+            logger.error("Cannot decode as json array from \(urlString)")
+            throw URLError(.cannotDecodeRawData)
+        }
+        return jsonArray
+    }
+    
+    func image(urlString: String) async throws -> UIImage {
+        let data = try await provider.data(urlString: urlString)
+        guard let image = UIImage(data: data) else {
+            logger.error("Cannot decode as image from \(urlString)")
+            throw URLError(.cannotDecodeRawData)
+        }
+        return image
+    }
+}
