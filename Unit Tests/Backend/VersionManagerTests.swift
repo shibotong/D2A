e1dbf9904c75ba277d0b05ea58da2c8748a80e9e
Shibo Tong <shibo.tong@icloud.com>
2025-12-11 22:46:38 +1100

    create version manager (#181)
    * create app logger
    
    * update hero functions
    
    * change version and update version check
    
    * remove some code

---
+++ Unit Tests/Backend/VersionManagerTests.swift
@@ -0,0 +1,33 @@
+//
+//  VersionManagerTests.swift
+//  D2A
+//
+//  Created by Shibo Tong on 11/12/2025.
+//
+
+import Testing
+@testable import D2A
+
+struct VersionManagerTests {
+    
+    @Test("Test update")
+    func versionUpdate() {
+        let manager = VersionManager(lastVersion: "1.0.0", appVersion: "1.1.1")
+        #expect(manager.isNewInstalled == false)
+        #expect(manager.isUpdated == true)
+    }
+    
+    @Test("Test newly installed")
+    func newlyInstalled() {
+        let manager = VersionManager(lastVersion: nil, appVersion: "1.1.1")
+        #expect(manager.isNewInstalled == true)
+        #expect(manager.isUpdated == false)
+    }
+    
+    @Test("Test not updated")
+    func noChange() {
+        let manager = VersionManager(lastVersion: "1.1.1", appVersion: "1.1.1")
+        #expect(manager.isNewInstalled == false)
+        #expect(manager.isUpdated == false)
+    }
+}
