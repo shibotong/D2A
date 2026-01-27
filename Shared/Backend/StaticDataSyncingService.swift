a0e7441fbf822e420cfc708d7383de0c0398ab37
Shibo Tong <shibo.tong@icloud.com>
2026-01-27 17:39:56 +1100

    update syncing service

--- Shared/Backend/StaticDataSyncingService.swift
+++ Shared/Backend/StaticDataSyncingService.swift
@@ -5,17 +5,62 @@
 //  Created by Shibo Tong on 27/1/2026.
 //
 
+import Foundation
+import CoreData
+import Logging
+
 class StaticDataSyncingService {
     
     private let openDota: OpenDotaFetching
     
+    /// Private context for saving static data
+    private let context: NSManagedObjectContext
+    
+    private let logger: Logger
+    
     init(openDota: OpenDotaFetching = OpenDotaController.shared,
-         persistance: PersistanceController) {
+         persistance: PersistanceProviding = PersistanceController.shared,
+         logger: Logger = D2ALogger.syncing) {
         self.openDota = openDota
+        self.context = persistance.mainContext.makeContext(author: "Static Data")
+        self.logger = logger
     }
     
     func startSyncing() async {
-        let context = persistenceController.shared.makeContext()
         
+    }
+    
+    private func syncAbilities() async throws {
+        logger.trace("Start syncing abilities")
+        async let abilityIDAsync = openDota.constants(service: .abilityIDs) as? [String: String]
+        async let abilitiesAsync = openDota.constants(service: .abilities) as? [String: Any]
+        let (abilityIDs, abilities) = try await (abilityIDAsync, abilitiesAsync)
+        guard let abilityIDs, let abilities else {
+            throw URLError(.badServerResponse)
+        }
+        logger.trace("Finish fetching abilityIDs and abilities from OpenDota")
+        for (abilityIDString, name) in abilityIDs {
+            logger.trace("processing ability: \(abilityIDString) \(name)")
+            guard let ability = abilities[name] as? [String: Any] else {
+                logger.info("Not able to find abiilty from data: \(name)")
+                continue
+            }
+            var abilityIDString = abilityIDString
+            if abilityIDString == "3060,1617" {
+                abilityIDString = "1617"
+            }
+            guard let abilityID = Int(abilityIDString) else {
+                logger.error("Ability ID is not an integer: \(abilityIDString)")
+                continue
+            }
+            let context = context.makeContext(author: "ability \(abilityID)")
+            do {
+                try await context.perform {
+                    try Ability.save(id: abilityID, name: name, data: ability, in: context)
+                }
+            } catch {
+                continue
+            }
+        }
     }
 }
