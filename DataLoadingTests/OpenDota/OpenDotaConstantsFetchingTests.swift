754273e4b1930567a95563f38ada774d1d2bbce0
Shibo Tong <shibo.tong@icloud.com>
2026-03-24 16:36:46 +1100

    create a fetcher to fetch heroes

---
+++ DataLoadingTests/OpenDota/OpenDotaConstantsFetchingTests.swift
@@ -0,0 +1,25 @@
+//
+//  OpenDotaConstantsFetchingTests.swift
+//  Unit Tests
+//
+//  Created by Shibo Tong on 24/3/2026.
+//
+
+import Testing
+@testable import D2A
+
+struct OpenDotaConstantsFetchingTests {
+    
+    let fetcher: OpenDotaConstantFetcher
+    
+    init() {
+        fetcher = OpenDotaConstantFetcher()
+    }
+
+    @Test
+    func `fetch heroes`() async throws {
+        let heroes = try await fetcher.heroes()
+        #expect(heroes.count != 0)
+    }
+
+}
---
+++ Shared/Backend/OpenDota/OpenDotaConstantFetcher.swift
@@ -0,0 +1,34 @@
+//
+//  OpenDotaConstantFetcher.swift
+//  D2A
+//
+//  Created by Shibo Tong on 24/3/2026.
+//
+
+import Foundation
+
+protocol OpenDotaConstantFetching {
+    func heroes() async throws -> [String: ODHero]
+}
+
+class OpenDotaConstantFetcher: OpenDotaConstantFetching {
+    
+    static let shared = OpenDotaConstantFetcher()
+    
+    private let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
+    private let baseURL = "https://api.opendota.com/api/constants"
+    
+    func heroes() async throws -> [String: ODHero] {
+        guard let url = URL(string: "\(baseURL)/heroes") else {
+            return [:]
+        }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let heroesDictionary: [String: ODHero] = try DecodingService().decode(data, decoder: decoder)
+        return heroesDictionary
+    }
+}
