ef9c38e9c3cc5949176c68fabe9acb5150c7dcc8
Shibo Tong <shibo.tong@icloud.com>
2026-03-24 22:52:15 +1100

    move files

---
+++ Shared/Backend/Stratz/Model/SKAbility.swift
@@ -0,0 +1,15 @@
+//
+//  SKAbility.swift
+//  D2A
+//
+//  Created by Shibo Tong on 24/3/2026.
+//
+
+struct SKAbility {
+    let id: Int
+    let displayName: String?
+    let lore: String?
+    let description: [String]
+    let aghanimDescription: String?
+    let shardDescription: String?
+}
