63e852404383d8d4c0b99084ff7896d69288898d
Shibo Tong <shibo.tong@icloud.com>
2026-03-25 16:46:55 +1100

    update path

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Add Secret
-echo '{ "stratzToken": "'"$STRATZTOKEN"'" }' > ../Shared/GraphQL/secrets.json
+echo '{ "stratzToken": "'"$STRATZTOKEN"'" }' > ../Resources/graphql/secrets.json
