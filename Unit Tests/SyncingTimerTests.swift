573c1f21b3738910c0a3af7836b86dcd40a46d99
Shibo Tong <shibo.tong@icloud.com>
2026-04-16 21:42:48 +1000

    update unit tests

--- Unit Tests/SyncingTimerTests.swift
+++ Unit Tests/SyncingTimerTests.swift
@@ -9,18 +9,36 @@ import Testing
 @testable import D2A
 import Foundation
 
-struct SyncingTimerTests {
+class MockUserDefaults: UserDefaults {
+    var persistedValue: Date? = nil
+        
+    override func set(_ value: Any?, forKey defaultName: String) {
+        persistedValue = value as? Date
+    }
+    
+    override func value(forKey key: String) -> Any? {
+        return persistedValue
+    }
+}
+
+class SyncingTimerTests {
+    
+    let userDefaults: UserDefaults
+    
+    init() {
+        userDefaults = MockUserDefaults()
+    }
     
     @Test
     func `should sync when no date set`() {
-        let syncingTimer = SyncingTimer()
+        let syncingTimer = SyncingTimer(userDefaults: userDefaults)
         let shouldSync = syncingTimer.shouldSync(key: .constants)
         #expect(shouldSync == true)
     }
     
     @Test
     func `should sync when date set to yesterday`() {
-        let syncingTimer = SyncingTimer {
+        let syncingTimer = SyncingTimer(userDefaults: userDefaults) {
             let date = Date()
             return date.addingTimeInterval(-24 * 60 * 60)
         }
@@ -31,7 +49,7 @@ struct SyncingTimerTests {
     
     @Test
     func `should not sync when date set to today`() {
-        let syncingTimer = SyncingTimer()
+        let syncingTimer = SyncingTimer(userDefaults: userDefaults)
         syncingTimer.finishSyncing(key: .constants)
         let shouldSync = syncingTimer.shouldSync(key: .constants)
         #expect(shouldSync == false)
