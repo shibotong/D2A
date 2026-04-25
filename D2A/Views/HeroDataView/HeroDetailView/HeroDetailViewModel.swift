7e4bdbedda0f09d1490fe0b7cd5875e5ea5d281e
Shibo Tong <shibo.tong@icloud.com>
2026-04-25 15:30:59 +1000

    Update HeroDetailViewModel.swift

--- D2A/Views/HeroDataView/HeroDetailView/HeroDetailViewModel.swift
+++ D2A/Views/HeroDataView/HeroDetailView/HeroDetailViewModel.swift
@@ -12,72 +12,36 @@ import StratzAPI
 import Apollo
 
 class HeroDetailViewModel: ObservableObject {
-    @Published var hero: any HeroProtocol
     
-    let name: String
-    let primaryAttribute: String
-    let localizedName: String
-    let complexity: Int
-    
+    let hero: Hero
     let abilities: [Ability]
     
-    let carryValue: Int
-    let disablerValue: Int
-    let escapeValue: Int
-    let supportValue: Int
-    let junglerValue: Int
-    let pusherValue: Int
-    let nukerValue: Int
-    let durableValue: Int
-    let initiatorValue: Int
-    
     @Published var selectedAbility: Ability?
-    
-    @Published var heroID: Int
-    
-    @Published var previousHeroID: Int?
-    @Published var nextHeroID: Int?
-    
-    
-    @Published var talent1Left: String = ""
-    @Published var talent2Left: String = ""
-    @Published var talent3Left: String = ""
-    @Published var talent4Left: String = ""
-    @Published var talent1Right: String = ""
-    @Published var talent2Right: String = ""
-    @Published var talent3Right: String = ""
-    @Published var talent4Right: String = ""
-    
     @Published var heroLevel = 1.00
+    @Published var hp: Int
+    @Published var hpRegen: Double
+    @Published var mana: Int
+    @Published var manaRegen: Double
+    @Published var strength: Int
+    @Published var agility: Int
+    @Published var intelligence: Int
     
-    private var database: HeroDatabase = HeroDatabase.shared
-    private let persistence: DataPersistenceService
-    private let context: NSManagedObjectContext
-    private let language: DataLanguageEnum
-    
-    init(hero: any HeroProtocol,
-         language: DataLanguageEnum = AppConfig.shared.languageCode,
-         context: NSManagedObjectContext = PersistenceProvider.shared.mainContext,
-         persistence: DataPersistenceService = .shared) {
+    init(hero: Hero, abilities: [Ability]) {
         self.hero = hero
-        heroID = hero.heroID
-        self.persistence = persistence
-        self.context = context
-        self.language = language
-        self.abilities = hero.heroAbilities
-        self.name = hero.heroName
-        self.primaryAttribute = hero.primaryAttribute
-        self.localizedName = hero.localizedName
-        self.complexity = Int(hero.hero.complexity)
-        self.carryValue = Int(hero.hero.roleCarry)
-        self.disablerValue = Int(hero.hero.roleDisabler)
-        self.escapeValue = Int(hero.hero.roleEscape)
-        self.supportValue = Int(hero.hero.roleSupport)
-        self.junglerValue = Int(hero.hero.roleJungler)
-        self.pusherValue = Int(hero.hero.rolePusher)
-        self.nukerValue = Int(hero.hero.roleNuker)
-        self.durableValue = Int(hero.hero.roleDurable)
-        self.initiatorValue = Int(hero.hero.roleInitiator)
+        self.abilities = abilities
+        
+        hp = hero.calculateHPLevel(level: 1)
+        hpRegen = hero.calculateHPRegen(level: 1)
+        mana = hero.calculateManaLevel(level: 1)
+        manaRegen = hero.calculateMPRegen(level: 1)
+        strength = hero.calculateAttribute(level: 1, attr: .str)
+        agility = hero.calculateAttribute(level: 1, attr: .agi)
+        intelligence = hero.calculateAttribute(level: 1, attr: .int)
+    }
+    
+    convenience init(hero: any HeroProtocol) {
+        self.init(hero: hero.hero,
+                  abilities: hero.heroAbilities)
     }
     
     convenience init(heroID: Int,
@@ -85,6 +49,10 @@ class HeroDetailViewModel: ObservableObject {
                      context: NSManagedObjectContext = PersistenceProvider.shared.mainContext,
                      persistence: DataPersistenceService = .shared) {
         let hero = (try? persistence.fetch(heroID: heroID, context: context)) ?? Hero(context: context)
-        self.init(hero: hero, language: language, context: context, persistence: persistence)
+        self.init(hero: hero)
+    }
+    
+    private func setupBinding() {
+        
     }
 }
