bad7a7accc9d06186d378eeadc903bbea8bf80c2
Shibo Tong <shibo.tong@icloud.com>
2026-04-25 14:36:43 +1000

    feat(ability): migrate to view only (#210)

--- D2A/Views/HeroDataView/AbilityView/AbilityView.swift
+++ D2A/Views/HeroDataView/AbilityView/AbilityView.swift
@@ -27,25 +27,85 @@ enum ScepterType: String {
 }
 
 struct AbilityView: View {
-    @Environment(\.dismiss) var dismiss
-    @ObservedObject var viewModel: AbilityViewModel
+    
+    let heroName: String
+    
+    // AbilityTitleView
+    let displayName: String
+    let cd: String?
+    let mc: String?
+    let name: String?
+    
+    // AbilityStatsView
+    let behavior: String?
+    let targetTeam: String?
+    let bkbPierce: String?
+    let dispellable: String?
+    let damageType: String?
 
+    let description: String?
+    let scepter: String?
+    let shard: String?
+    
+    let lore: String?
+    let attributes: [AbilityTranslation.Attribute]?
+    
+    // AbilityDescriptionView
+    @State var scepterVideo: AVPlayer?
+    @State var shardVideo: AVPlayer?
+    @State var abilityVideo: AVPlayer?
+    
+    init(heroName: String, ability: Ability) {
+        self.init(heroName: heroName, displayName: ability.displayName, cd: ability.coolDown,
+                  mc: ability.manaCost, name: ability.name, behavior: ability.behavior,
+                  targetTeam: ability.targetTeam, bkbPierce: ability.bkbPierce, dispellable: ability.dispellable,
+                  damageType: ability.damageType, description: ability.desc, scepter: ability.scepter, shard: ability.shard,
+                  lore: ability.lore, attributes: ability.localizedAttributes)
+    }
+    
+    init(heroName: String, displayName: String, cd: String?,
+         mc: String?, name: String?, behavior: String?,
+         targetTeam: String?, bkbPierce: String?, dispellable: String?,
+         damageType: String?, scepterVideo: AVPlayer? = nil,
+         shardVideo: AVPlayer? = nil, abilityVideo: AVPlayer? = nil,
+         description: String?, scepter: String?, shard: String?,
+         lore: String?, attributes: [AbilityTranslation.Attribute]?) {
+        self.heroName = heroName
+        self.displayName = displayName
+        self.cd = cd
+        self.mc = mc
+        self.name = name
+        self.behavior = behavior
+        self.targetTeam = targetTeam
+        self.bkbPierce = bkbPierce
+        self.dispellable = dispellable
+        self.damageType = damageType
+        self.scepterVideo = scepterVideo
+        self.shardVideo = shardVideo
+        self.abilityVideo = abilityVideo
+        self.description = description
+        self.scepter = scepter
+        self.shard = shard
+        self.lore = lore
+        self.attributes = attributes
+    }
+    
     var body: some View {
         GeometryReader { proxy in
             ScrollView(.vertical, showsIndicators: false) {
                 VStack {
-                    AbilityTitleView(displayName: viewModel.displayName,
-                                     cd: viewModel.cd,
-                                     mc: viewModel.mc,
-                                     name: viewModel.name ?? "")
-                    AbilityStatsView(behavior: viewModel.behavior,
-                                     targetTeam: viewModel.targetTeam,
-                                     bkbPierce: viewModel.bkbPierce, 
-                                     dispellable: viewModel.dispellable,
-                                     damageType: viewModel.damageType)
+                    AbilityTitleView(displayName: displayName,
+                                     cd: cd,
+                                     mc: mc,
+                                     name: name ?? "")
+                    AbilityStatsView(behavior: behavior,
+                                     targetTeam: targetTeam,
+                                     bkbPierce: bkbPierce,
+                                     dispellable: dispellable,
+                                     damageType: damageType)
                     buildDescription(proxy: proxy)
                     Spacer().frame(height: 10)
-                    if let attributes = viewModel.attributes {
+                    if let attributes {
                         HStack {
                             VStack(alignment: .leading, spacing: 5) {
                                 ForEach(attributes, id: \.self) { item in
@@ -56,7 +116,7 @@ struct AbilityView: View {
                         }
                     }
                     Spacer().frame(height: 10)
-                    if let lore = viewModel.lore {
+                    if let lore {
                         Text(lore)
                             .font(.system(size: 10))
                             .padding(8)
@@ -72,41 +132,99 @@ struct AbilityView: View {
         }
         .padding(.horizontal)
         .navigationBarTitleDisplayMode(.inline)
+        .task {
+            buildDetailView()
+        }
     }
     
     @ViewBuilder
     private func buildDescription(proxy: GeometryProxy) -> some View {
         VStack {
-            if let description = viewModel.description {
-                AbilityDescriptionView(width: proxy.size.width, type: .non, description: description, player: viewModel.abilityVideo)
+            if let description {
+                AbilityDescriptionView(width: proxy.size.width, type: .non, description: description, player: abilityVideo)
             }
-            if let scepter = viewModel.scepter {
-                AbilityDescriptionView(width: proxy.size.width, type: .scepter, description: scepter, player: viewModel.scepterVideo)
+            if let scepter {
+                AbilityDescriptionView(width: proxy.size.width, type: .scepter, description: scepter, player: scepterVideo)
             }
-            if let shard = viewModel.shard {
-                AbilityDescriptionView(width: proxy.size.width, type: .shard, description: shard, player: viewModel.shardVideo)
+            if let shard {
+                AbilityDescriptionView(width: proxy.size.width, type: .shard, description: shard, player: shardVideo)
             }
         }
+    }
+    
+    func buildDetailView() {
+        var ability: AVAsset?
+        var scepter: AVAsset?
+        var shard: AVAsset?
+        guard let abilityName = name else { return }
+        if abilityVideo == nil {
+            ability = getVideoURL(abilityName, type: .non)
+        }
+        if scepterVideo == nil {
+            scepter = getVideoURL(abilityName, type: .scepter)
+        }
+        if shardVideo == nil {
+            shard = getVideoURL(abilityName, type: .shard)
+        }
+        setVideoURL(ability: ability, scepter: scepter, shard: shard)
+    }
+    
+    @MainActor
+    private func setVideoURL(ability: AVAsset?, scepter: AVAsset?, shard: AVAsset?) {
+        if let ability {
+            let player = AVPlayer(playerItem: AVPlayerItem(asset: ability))
+            player.isMuted = true
+            abilityVideo = player
+        }
+        if let scepter {
+            let player = AVPlayer(playerItem: AVPlayerItem(asset: scepter))
+            player.isMuted = true
+            scepterVideo = player
+        }
+        if let shard {
+            let player = AVPlayer(playerItem: AVPlayerItem(asset: shard))
+            player.isMuted = true
+            shardVideo = player
+        }
+    }
+    
+    private func getVideoURL(_ ability: String, type: ScepterType) -> AVAsset? {
+        let trimmedHeroName = heroName.replacingOccurrences(of: "npc_dota_hero_", with: "")
+        let baseURL = "\(IMAGE_PREFIX)/apps/dota2/videos/dota_react/abilities/\(trimmedHeroName)"
+        var path = ""
+        switch type {
+        case .scepter:
+            path = "\(baseURL)/\(trimmedHeroName)_aghanims_scepter.mp4"
+        case .shard:
+            path = "\(baseURL)/\(trimmedHeroName)_aghanims_shard.mp4"
+        case .non:
+            path = "\(baseURL)/\(ability).mp4"
+        }
+
+        guard let video = CacheVideo.shared.getVideo(key: path, name: heroName) else {
+            return nil
+        }
+        return video.isPlayable ? video: nil
     }
 }
 
 #Preview("Blink") {
-    AbilityView(viewModel: .init(heroID: 1, ability: PreviewData.PreviewAbility.blink))
+    AbilityView(heroName: "npc_dota_hero_antimage", ability: PreviewData.PreviewAbility.blink)
         .environmentObject(PreviewData.environment)
 }
     
 #Preview("Counterspell") {
-    AbilityView(viewModel: .init(heroID: 1, ability: PreviewData.PreviewAbility.counterspell))
+    AbilityView(heroName: "npc_dota_hero_antimage", ability: PreviewData.PreviewAbility.counterspell)
         .environmentObject(PreviewData.environment)
 }
 
 #Preview("Mana Break") {
-    AbilityView(viewModel: .init(heroID: 1, ability: PreviewData.PreviewAbility.manaBreak))
+    AbilityView(heroName: "npc_dota_hero_antimage", ability: PreviewData.PreviewAbility.manaBreak)
         .environmentObject(PreviewData.environment)
 }
 
 #Preview("Mana Void") {
-    AbilityView(viewModel: .init(heroID: 1, ability: PreviewData.PreviewAbility.manaVoid))
+    AbilityView(heroName: "npc_dota_hero_antimage", ability: PreviewData.PreviewAbility.manaVoid)
         .environmentObject(PreviewData.environment)
 }
 
