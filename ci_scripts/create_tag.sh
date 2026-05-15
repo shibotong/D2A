5fbee01b23954044dc09cedd20f11ce2afb20a34
Shibo Tong <shibo.tong@icloud.com>
2026-05-15 16:26:45 +1000

    push tag

--- ci_scripts/create_tag.sh
+++ ci_scripts/create_tag.sh
@@ -2,10 +2,9 @@
 
 echo 'Creating Tags'
 
-VERSION=$(cat ../D2A.xcodeproj/project.pbxproj | grep -m3 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ' | tail -n 1)
+VERSION=v$(cat ../D2A.xcodeproj/project.pbxproj | grep -m3 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ' | tail -n 1)
 
 echo $VERSION
 
-git tag v${VERSION}
 
-git push --tags https://${GIT_ACCOUNT}:${GIT_AUTH_TOKEN}@github.com/${GIT_ACCOUNT}/${CI_PRODUCT}.git
+curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GIT_AUTH_TOKEN}" https://api.github.com/repos/${GIT_ACCOUNT}/${CI_PRODUCT}/releases -d "{\"tag_name\": \"${VERSION}\",\"name\": \"${VERSION}\", \"generate_release_notes\": true, \"make_lates\": true}"
