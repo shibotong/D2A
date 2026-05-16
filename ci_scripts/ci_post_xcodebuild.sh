abc63c3ed453db63e2d0a586be2b22b910a83a17
Shibo Tong <shibo.tong@icloud.com>
2026-05-16 12:04:14 +1000

    temp fix for list tags

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -2,14 +2,18 @@
 
 set -e
 
-echo "Running workflow: $CI_WORKFLOW"
-
-if [ "$CI_WORKFLOW" != 'Submit to App Store' ]
-then
-    exit 0
-fi
-
-if [[ -d $CI_APP_STORE_SIGNED_APP_PATH ]]
-then
-    ./create_release.sh
-fi
+#echo "Running workflow: $CI_WORKFLOW"
+#
+#if [ "$CI_WORKFLOW" != 'Submit to App Store' ]
+#then
+#    exit 0
+#fi
+#
+#if [[ -d $CI_APP_STORE_SIGNED_APP_PATH ]]
+#then
+#    ./create_release.sh
+#fi
+echo "Git tags"
+TAGS=$(git tags)
+echo $TAGS
+#./create_release.sh
