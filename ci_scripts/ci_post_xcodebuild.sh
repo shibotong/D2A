09bf52747cc298a5ce705ffba2f892fe077674bc
Shibo Tong <shibo.tong@icloud.com>
2026-05-19 20:14:48 +1000

    Update ci_post_xcodebuild.sh

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,14 +4,12 @@ set -e
 
 echo "Running workflow: $CI_WORKFLOW"
 
-#if [ "$CI_WORKFLOW" != 'Submit to App Store' ]
-#then
-#    exit 0
-#fi
-#
-#if [[ -d $CI_APP_STORE_SIGNED_APP_PATH ]]
-#then
-#    ./create_release.sh
-#fi
+if [ "$CI_WORKFLOW" != 'Submit to App Store' ]
+then
+    exit 0
+fi
 
-./create_release.sh
+if [[ -d $CI_APP_STORE_SIGNED_APP_PATH ]]
+then
+    ./create_release.sh
+fi
