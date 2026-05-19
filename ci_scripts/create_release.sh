a4ad6196daed9b84571f3d3be3622eab2c078577
Shibo Tong <shibo.tong@icloud.com>
2026-05-19 19:33:41 +1000

    remove auth token

--- ci_scripts/create_release.sh
+++ ci_scripts/create_release.sh
@@ -2,4 +2,4 @@
 
 VERSION=v$(cat ../D2A.xcodeproj/project.pbxproj | grep -m3 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ' | tail -n 1)
 
-swift ./create_release.swift ${GIT_ACCOUNT} ${CI_PRODUCT} ${GIT_AUTH_TOKEN} ${VERSION}
+swift ./create_release.swift ${GIT_ACCOUNT} ${CI_PRODUCT} ${VERSION}
