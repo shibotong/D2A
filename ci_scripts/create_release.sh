18e980d228ca1836cb5193c7a6297ac280cf1700
Shibo Tong <shibo.tong@icloud.com>
2026-05-19 20:03:21 +1000

    update script

--- ci_scripts/create_release.sh
+++ ci_scripts/create_release.sh
@@ -2,10 +2,4 @@
 
 VERSION=v$(cat ../D2A.xcodeproj/project.pbxproj | grep -m3 'MARKETING_VERSION' | cut -d'=' -f2 | tr -d ';' | tr -d ' ' | tail -n 1)
 
-echo "this is user name"
-echo $GIT_USER_NAME
-
-echo "this is ci product"
-echo $CI_PRODUCT
-
-swift ./create_release.swift $GIT_USER_NAME ${CI_PRODUCT} $GIT_AUTH_TOKEN ${VERSION}
+swift ./create_release.swift ${GIT_USER_NAME} ${CI_PRODUCT} ${GIT_AUTH_TOKEN} ${VERSION}
